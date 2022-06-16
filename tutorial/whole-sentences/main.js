7ccc9af392796a731124c3ac68ae2ff0499d7a86
JP McDowell <jp.mcdowell@gmail.com>
2022-06-15 23:46:30 -0700

    Commit me

--- tutorial/whole-sentences/main.js
+++ tutorial/whole-sentences/main.js
@@ -1,4 +1,169 @@
-jQuery(function($) {
-  'use strict';
+const inputTxt = document.querySelector(".txt");
 
-});
+let previousOrNext = 0;
+let howMany = read_sentences(whole_sentences);
+
+inputTxt.value = (howMany.length === 0) ? "" : howMany[previousOrNext];
+
+const one = document.querySelector("#one");
+
+function setText() {
+  one.value = "";
+  return inputTxt.value = howMany[previousOrNext];
+}
+
+function play() {
+  if (one.value !== "") {
+    speak(one.value);
+    inputTxt.blur();
+  }
+}
+
+function read_sentences(ws) {
+
+  let url = new URL(document.location);
+  let items = [];
+
+  const paramsString = url.search;
+  const searchParams = new URLSearchParams(paramsString);
+
+  if (searchParams.get("id")) {
+
+    searchId = searchParams.get("id");
+
+    for (let i=0; i<ws.length; i++) {
+      if (searchId === i.toString()) {
+        items = ws[i];
+      }
+    }
+
+    for (let i=0; i<items.length; i++) {
+      items[i] = shuffle(items[i])
+    }
+  }
+
+  return items;
+}
+
+// This is all the same down here
+
+howMany = shuffle(howMany);
+
+function previous() {
+  if (previousOrNext <= 0) {
+    previousOrNext = howMany.length;
+  }
+  previousOrNext--;
+  return setText();
+}
+
+function next() {
+  if (previousOrNext >= howMany.length - 1) {
+    previousOrNext = -1;
+  }
+  previousOrNext++;
+  return setText();
+}
+
+function shuffle(array) {
+
+  let items = JSON.parse(JSON.stringify(array));
+  let currentIndex = items.length, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [items[currentIndex], items[randomIndex]] = [
+      items[randomIndex], items[currentIndex]];
+  }
+
+  return items;
+}
+
+// "Microsoft Zira - English (United States)"
+// "Samantha"
+
+const synth = window.speechSynthesis;
+const voiceSelect = document.querySelector("select");
+let voices = [];
+
+const pitch = document.querySelector("#pitch");
+const rate = document.querySelector("#rate");
+
+function populateVoiceList() {
+
+  // Sorted by
+  voices = synth.getVoices().sort(function (a, b) {
+    const aname = a.name.toUpperCase();
+    const bname = b.name.toUpperCase();
+    if (aname < bname) {
+      return -1;
+    } else if (aname == bname) {
+      return 0;
+    } else {
+      return +1;
+    }
+  });
+
+  voiceSelect.innerHTML = "";
+  for (let i = 0; i < voices.length; i++) {
+    if (voices[i].lang === "en-US") {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      if (voices[i].name === "Microsoft Zira - English (United States)") {
+        option.defaultSelected = true;
+      }
+      else if (voices[i].name === "Samantha") {
+        option.defaultSelected = true;
+      }
+      voiceSelect.appendChild(option);
+    }
+  }
+}
+
+populateVoiceList();
+
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+}
+
+function speak(talk) {
+
+  if (synth.speaking) {
+    console.error("speechSynthesis.speaking");
+    return;
+  }
+
+  if (inputTxt.value !== "") {
+    const utterThis = new SpeechSynthesisUtterance(talk);
+
+    utterThis.onend = function (event) {
+      console.log("SpeechSynthesisUtterance.onend");
+    };
+
+    utterThis.onerror = function (event) {
+      console.error("SpeechSynthesisUtterance.onerror");
+    };
+
+    const selectedOption =
+      voiceSelect.selectedOptions[0].getAttribute("data-name");
+
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+        break;
+      }
+    }
+
+    utterThis.pitch = pitch.value;
+    utterThis.rate = rate.value;
+    synth.speak(utterThis);
+  }
+}
--- tutorial/write-words/main.js
+++ tutorial/write-words/main.js
@@ -1,151 +1,156 @@
-//jQuery(function($) {
-//  'use strict';
+const inputTxt = document.querySelector(".txt");
 
-  function words_push(words) {
+let previousOrNext = 0;
+let howMany = read_sentences(words);
 
-    let items = [];
-    for (const i of words) {
-      for (const j of i) {
-        items.push(j);
-      }
+inputTxt.value = '-----';
+
+const one = document.querySelector("#one");
+
+function setText() {
+  one.value = "";
+  return inputTxt.value = '-----';
+}
+
+function show() {
+  inputTxt.value = howMany[previousOrNext].word;
+}
+
+function play() {
+  speak(howMany[previousOrNext].word);
+  inputTxt.blur();
+}
+
+function read_sentences(w) {
+
+  let items = [];
+  for (const i of w) {
+    for (const j of i) {
+      items.push(j);
     }
-    return items;
-  };
+  }
+  return items;
+};
 
-  let a = words_push(words);
+// This is all the same down here
 
-  function shuffle(array) {
+howMany = shuffle(howMany);
 
-    let items = JSON.parse(JSON.stringify(array));
-    let currentIndex = items.length, randomIndex;
+function previous() {
+  if (previousOrNext <= 0) {
+    previousOrNext = howMany.length;
+  }
+  previousOrNext--;
+  return setText();
+}
 
-    // While there remain elements to shuffle...
-    while (currentIndex !== 0) {
+function next() {
+  if (previousOrNext >= howMany.length - 1) {
+    previousOrNext = -1;
+  }
+  previousOrNext++;
+  return setText();
+}
 
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
+function shuffle(array) {
 
-      // And swap it with the current element.
-      [items[currentIndex], items[randomIndex]] = [
-        items[randomIndex], items[currentIndex]];
+  let items = JSON.parse(JSON.stringify(array));
+  let currentIndex = items.length, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [items[currentIndex], items[randomIndex]] = [
+      items[randomIndex], items[currentIndex]];
+  }
+
+  return items;
+}
+
+// "Microsoft Zira - English (United States)"
+// "Samantha"
+
+const synth = window.speechSynthesis;
+const voiceSelect = document.querySelector("select");
+let voices = [];
+
+const pitch = document.querySelector("#pitch");
+const rate = document.querySelector("#rate");
+
+function populateVoiceList() {
+
+  // Sorted by
+  voices = synth.getVoices().sort(function (a, b) {
+    const aname = a.name.toUpperCase();
+    const bname = b.name.toUpperCase();
+    if (aname < bname) {
+      return -1;
+    } else if (aname == bname) {
+      return 0;
+    } else {
+      return +1;
     }
+  });
 
-    return items;
-  };
-
-  let b = shuffle(a);
-
-  let i = 0;
-  let j = '-----';
-  const inputTxt = document.querySelector(".txt");
-  const one = document.querySelector("#one");
-  inputTxt.value = j;
-
-  function previous() {
-    if (i <= 0) i = b.length;
-    i--;
-    return setText();
-  }
-
-  function next() {
-    if (i >= b.length - 1) i = -1;
-    i++;
-    return setText();
-  }
-
-  function setText() {
-    one.value = "";
-    return inputTxt.value = j;
-  }
-
-  function show() {
-    inputTxt.value = b[i].word;
-  }
-
-  const synth = window.speechSynthesis;
-  const voiceSelect = document.querySelector("select");
-  const pitch = document.querySelector("#pitch");
-  const rate = document.querySelector("#rate");
-
-  let voices = [];
-
-  // "Microsoft Zira - English (United States)"
-  // "Samantha"
-
-  function populateVoiceList() {
-
-    voices = synth.getVoices().sort(function (a, b) {
-      const aname = a.name.toUpperCase();
-      const bname = b.name.toUpperCase();
-      if (aname < bname) {
-        return -1;
-      } else if (aname == bname) {
-        return 0;
-      } else {
-        return +1;
+  voiceSelect.innerHTML = "";
+  for (let i = 0; i < voices.length; i++) {
+    if (voices[i].lang === "en-US") {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      if (voices[i].name === "Microsoft Zira - English (United States)") {
+        option.defaultSelected = true;
       }
-    });
+      else if (voices[i].name === "Samantha") {
+        option.defaultSelected = true;
+      }
+      voiceSelect.appendChild(option);
+    }
+  }
+}
 
-    voiceSelect.innerHTML = "";
+populateVoiceList();
+
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+}
+
+function speak(talk) {
+
+  if (synth.speaking) {
+    console.error("speechSynthesis.speaking");
+    return;
+  }
+
+  if (inputTxt.value !== "") {
+    const utterThis = new SpeechSynthesisUtterance(talk);
+
+    utterThis.onend = function (event) {
+      console.log("SpeechSynthesisUtterance.onend");
+    };
+
+    utterThis.onerror = function (event) {
+      console.error("SpeechSynthesisUtterance.onerror");
+    };
+
+    const selectedOption =
+      voiceSelect.selectedOptions[0].getAttribute("data-name");
+
     for (let i = 0; i < voices.length; i++) {
-      if (voices[i].lang === "en-US") {
-        const option = document.createElement("option");
-        option.textContent = `${voices[i].name} (${voices[i].lang})`;
-        option.setAttribute("data-lang", voices[i].lang);
-        option.setAttribute("data-name", voices[i].name);
-        if ((voices[i].name === "Microsoft Zira - English (United States)") || (voices[i].name === "Samantha")) {
-          option.defaultSelected = true;
-        }
-        voiceSelect.appendChild(option);
+      if (voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+        break;
       }
     }
+
+    utterThis.pitch = pitch.value;
+    utterThis.rate = rate.value;
+    synth.speak(utterThis);
   }
-
-  populateVoiceList();
-
-  if (speechSynthesis.onvoiceschanged !== undefined) {
-    speechSynthesis.onvoiceschanged = populateVoiceList;
-  }
-
-  function speak() {
-
-    if (synth.speaking) {
-      console.error("speechSynthesis.speaking");
-      return;
-    }
-
-    if (inputTxt.value !== "") {
-      const utterThis = new SpeechSynthesisUtterance(b[i].word);
-
-      //utterThis.onend = function (event) {
-      //  console.log("SpeechSynthesisUtterance.onend");
-      //};
-
-      utterThis.onerror = function (event) {
-        console.error("SpeechSynthesisUtterance.onerror");
-      };
-
-      const selectedOption =
-        voiceSelect.selectedOptions[0].getAttribute("data-name");
-
-      for (let i = 0; i < voices.length; i++) {
-        if (voices[i].name === selectedOption) {
-          utterThis.voice = voices[i];
-          break;
-        }
-      }
-
-      utterThis.pitch = pitch.value;
-      utterThis.rate = rate.value;
-      synth.speak(utterThis);
-    }
-  }
-
-  function play() {
-
-    speak();
-    inputTxt.blur();
-  }
-
-//});
+}
