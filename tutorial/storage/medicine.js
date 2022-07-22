c8ad3db53146ea29a1cc3b45e77bc314a71f0dac
JP McDowell <jp.mcdowell@gmail.com>
2022-07-21 18:17:45 -0700

    Commit me

---
+++ tutorial/storage/medicine.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const words = [
+  { "word": "labetalol", "description": "Labetalol HCL 300 mg tablet" },
+  { "word": "lisinopril", "description": "Lisinopril 20 mg tablet" },
+  { "word": "amlodipine besylate", "description": "Amlodipine Besylate 10 mg tablet" },
+  { "word": "spironolactone", "description": "Spironolactone 25 mg tablet" },
+  { "word": "tamsulosin", "description": "Tamsulosin HCL 0.4 mg capsule" },
+  { "word": "rosuvastatin calcium", "description": "Rosuvastatin Calcium 5 mg tablet" },
+];
