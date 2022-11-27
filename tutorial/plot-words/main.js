c9eddee52a4482bf8327625decdae909159815a6
JP McDowell <jpedius@users.noreply.github.com>
2022-11-26 22:12:14 -0800

    Commit me

--- tutorial/plot-words/main.js
+++ tutorial/plot-words/main.js
@@ -4,7 +4,7 @@
 
   function plotTxt() {
  
-    let c = "/tutorial/storage/plot/" + ps.value + ".txt";
+    //let c = "/tutorial/storage/plot/" + letSelectPlot.value + ".txt";
     /*  
     let e = `The series follows the life of a boy named Harry Potter. In the first book, Harry Potter and the Philosopher's Stone, Harry lives in a cupboard under the stairs in the house of the Dursleys, his aunt, uncle and cousin. The Dursleys consider themselves perfectly normal, but at the age of eleven, Harry discovers that he is a wizard. He meets a half-giant named Hagrid who invites him to attend the Hogwarts School of Witchcraft and Wizardry. Harry learns that as a baby, his parents were murdered by the dark wizard Lord Voldemort. When Voldemort attempted to kill Harry, his curse rebounded and Harry survived with a lightning-shaped scar on his forehead.`
 
@@ -15,7 +15,7 @@
     
     console.log(e, d, howMany);
     */
-    $.get(c, function(data) {
+    $.get("/tutorial/storage/plot/" + letSelectPlot.value + ".txt", function(data) {
       
       let b = data.match( /[^\.!\?]+[\.!\?]+/g );
       b = b.map(x => x.trim());
@@ -55,14 +55,30 @@
   }
 
   let inputTxt = document.querySelector(".txt");
-  let ps = document.querySelector("#plot");
+  let letButtonPrev = document.querySelector("#buttonPrev");
+  let letButtonPlay = document.querySelector("#buttonPlay");
+  let letButtonNext = document.querySelector("#buttonNext");
+  let letSelectPlot = document.querySelector("#plot");
 
   let previousOrNext = 0;
   let howMany = ["Hello", "World"];
   inputTxt.value = howMany[previousOrNext];
   
   plotTxt();
-  ps.addEventListener('change', function() { plotTxt() }, false);
+  letSelectPlot.addEventListener('change', function() {
+    plotTxt()
+  }, false);
+  
+  letButtonPrev.addEventListener("click", function() {
+    previous();
+  }, false);
+  
+  letButtonPlay.addEventListener("click", function() {
+    play();
+  }, false); 
 
+  letButtonNext.addEventListener("click", function() {
+    next();
+  }, false); 
 
 })(jQuery);
