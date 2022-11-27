a0a62ad3f84c49f442e5123ebfec2a993d17b9b3
JP McDowell <jpedius@users.noreply.github.com>
2022-11-26 20:30:17 -0800

    Commit me

--- tutorial/plot-words/main.js
+++ tutorial/plot-words/main.js
@@ -5,7 +5,7 @@
   function plotTxt() {
  
     let c = "/tutorial/storage/plot/" + ps.value + ".txt";
-    
+    /*  
     let e = `The series follows the life of a boy named Harry Potter. In the first book, Harry Potter and the Philosopher's Stone, Harry lives in a cupboard under the stairs in the house of the Dursleys, his aunt, uncle and cousin. The Dursleys consider themselves perfectly normal, but at the age of eleven, Harry discovers that he is a wizard. He meets a half-giant named Hagrid who invites him to attend the Hogwarts School of Witchcraft and Wizardry. Harry learns that as a baby, his parents were murdered by the dark wizard Lord Voldemort. When Voldemort attempted to kill Harry, his curse rebounded and Harry survived with a lightning-shaped scar on his forehead.`
 
     let d = e.match( /[^\.!\?]+[\.!\?]+/g );
@@ -14,7 +14,7 @@
     previousOrNext = 0;
     
     console.log(e, d, howMany);
-    
+    */
     $.get(c, function(data) {
       
       let b = data.match( /[^\.!\?]+[\.!\?]+/g );
