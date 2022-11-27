6e8f0d330af275402426f461732d993e35cfb9c7
JP McDowell <jpedius@users.noreply.github.com>
2022-11-26 23:37:20 -0800

    Commit me

--- tutorial/plot-words/main.js
+++ tutorial/plot-words/main.js
@@ -2,30 +2,12 @@
 
 (function($) {
 
-  function plotTxt() {
- 
-    let c = "/tutorial/storage/plot/" + letSelectPlot.value + ".txt";
-    /*  
-    let e = `The series follows the life of a boy named Harry Potter. In the first book, Harry Potter and the Philosopher's Stone, Harry lives in a cupboard under the stairs in the house of the Dursleys, his aunt, uncle and cousin. The Dursleys consider themselves perfectly normal, but at the age of eleven, Harry discovers that he is a wizard. He meets a half-giant named Hagrid who invites him to attend the Hogwarts School of Witchcraft and Wizardry. Harry learns that as a baby, his parents were murdered by the dark wizard Lord Voldemort. When Voldemort attempted to kill Harry, his curse rebounded and Harry survived with a lightning-shaped scar on his forehead.`
-
-    let d = e.match( /[^\.!\?]+[\.!\?]+/g );
-    d = d.map(x => x.trim());
-    howMany = d;
-    previousOrNext = 0;
-    
-    console.log(e, d, howMany);
-    */
-    $.get(c, function(data) {
-      
-      let b = data.match( /[^\.!\?]+[\.!\?]+/g );
-      b = b.map(x => x.trim());
-      howMany = b;
-      previousOrNext = 0;
-      
+  function plot() {
+    let src = "/tutorial/storage/plot/" + letSelectPlot.value + ".txt";
+    $.get(src, function(data) {
+      previousOrNext = 0;     
+      howMany = data.match( /[^\.!\?]+[\.!\?]+/g ).map(x => x.trim()); 
       letSelectText.value = howMany[previousOrNext];
-      
-      console.log(data, b, howMany);
- 
     }, "text");
   };
 
@@ -55,30 +37,21 @@
   }
 
   let letSelectText = document.querySelector("#selectText");
-  let letButtonPrev = document.querySelector("#buttonPrev");
-  let letButtonPlay = document.querySelector("#buttonPlay");
-  let letButtonNext = document.querySelector("#buttonNext");
-  let letSelectPlot = document.querySelector("#plot");
-
   let previousOrNext = 0;
   let howMany = ["Hello", "World"];
   letSelectText.value = howMany[previousOrNext];
   
-  plotTxt();
-  letSelectPlot.addEventListener("change", function() {
-    plotTxt()
-  }, false);
-  
-  letButtonPrev.addEventListener("click", function() {
-    previous();
-  }, false);
-  
-  letButtonPlay.addEventListener("click", function() {
-    play();
-  }, false); 
+  let letButtonPrev = document.querySelector("#buttonPrev");
+  letButtonPrev.addEventListener("click", function() { previous() }, false);
 
-  letButtonNext.addEventListener("click", function() {
-    next();
-  }, false); 
+  let letButtonPlay = document.querySelector("#buttonPlay");
+  letButtonPlay.addEventListener("click", function() { play() }, false);
+
+  let letButtonNext = document.querySelector("#buttonNext");
+  letButtonNext.addEventListener("click", function() { next() }, false);
+
+  let letSelectPlot = document.querySelector("#selectPlot");
+  letSelectPlot.addEventListener("change", function() { plot() }, false);
+  plot();
 
 })(jQuery);
