8c42a2af85f8f9a279ae1bca50888dd24e034bae
JP McDowell <jpedius@users.noreply.github.com>
2022-11-28 01:30:57 -0800

    Commit me

--- games/main.js
+++ games/main.js
@@ -3,6 +3,8 @@
 (function($) {
 
   let b0 = document.querySelector(".a0");
-  b0.addEventListener("click", function() { alert(); }, false);
+  b0.addEventListener("click", function() {
+    b0.value = b0.value + "@";
+  }, false);
 
 })(jQuery);
