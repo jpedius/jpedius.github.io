c4047101d2013f299b71a59c962d5aefa003ab30
JP McDowell <jpedius@users.noreply.github.com>
2022-11-28 15:10:18 -0800

    Commit me

--- games/main.js
+++ games/main.js
@@ -8,7 +8,7 @@
   for (let d=0; d<9; d++) {
     let f = ".a" + d;
     let b0 = document.querySelector(f);
-    b0.innerHTML = " ";
+    b0.innerHTML = "_";
     e[d] = b0.addEventListener("click", function() {
       b0.innerHTML = "X";
     }, false);
