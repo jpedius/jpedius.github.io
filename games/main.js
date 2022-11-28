458b639d0547e557d5dcd78aa77ae46993ec23d0
JP McDowell <jpedius@users.noreply.github.com>
2022-11-28 14:15:08 -0800

    Commit me

--- games/main.js
+++ games/main.js
@@ -2,10 +2,15 @@
 
 (function($) {
 
-  let b0 = document.querySelector(".a0");
-  b0.innerHTML = "JP";
-  b0.addEventListener("click", function() {
-    b0.innerHTML = "Hello";
-  }, false);
+  let c = [" ", "X", "O"]; 
+
+  let e = [];
+  for (let d=0; d<9; d++) {
+    let b0 = document.querySelector(".a0");
+    b0.innerHTML = " ";
+    e[d] = b0.addEventListener("click", function() {
+      b0.innerHTML = "X";
+    }, false);
+  }
 
 })(jQuery);
