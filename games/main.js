0b91a1e082eb6e6900bd37e1038b6398491592b2
JP McDowell <jpedius@users.noreply.github.com>
2022-11-29 19:57:36 -0800

    Commit me

--- games/main.js
+++ games/main.js
@@ -42,8 +42,25 @@
   let t4  = document.createElement('div');
   t4.classList.add('text-a');
   
+  for (let t14=0; t14<9; t14++) {
+    let t15 = document.createElement('span');
+    t15.classList.add('ttt', 'a' + t14);
+    t15.innerHTML = '.a' + t14;
+    if (t14 < 3) {
+      t3.appendChild(t15);
+    } else if (t14 < 6) {
+      t4.appendChild(t15);
+    } else if (t14 < 9) {
+      t5.appendChild(t15);
+    }
+  }
+  
+  /*
+  
   let t5  = document.createElement('span');
   t5.classList.add('ttt', 'a0');
+  t5.innerHTML = '.a0';
+ 
   let t6  = document.createElement('span');
   t6.classList.add('ttt', 'a1');
   let t7  = document.createElement('span');
@@ -60,12 +77,14 @@
   t12.classList.add('ttt', 'a7');
   let t13 = document.createElement('span');
   t13.classList.add('ttt', 'a8');
+  */
+
   //t1.textContent = t0;
   
   //t2.textContent = t1;
   //t3.textContent = t1;
   //t4.textContent = t1; 
-  
+  /*
   t5.textContent = t2;
   t6.textContent = t2;
   t7.textContent = t2;
@@ -77,7 +96,8 @@
   t11.textContent = t4;
   t12.textContent = t4;
   t13.textContent = t4;  
-    
+  */  
+  /*
   t2.appendChild(t5);
   t2.appendChild(t6);
   t2.appendChild(t7);
@@ -89,6 +109,7 @@
   t4.appendChild(t11);
   t4.appendChild(t12);
   t4.appendChild(t13);    
+  */
   
   t1.appendChild(t2);
   t1.appendChild(t3);
