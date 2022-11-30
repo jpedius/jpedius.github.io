a6144ad617a42dd6a683df889c149813ffe1c21a
JP McDowell <jpedius@users.noreply.github.com>
2022-11-29 18:47:19 -0800

    Commit me

--- games/main.js
+++ games/main.js
@@ -33,33 +33,53 @@
   
   const t0 = document.querySelector('#root');
   
-  let t1 = document.createElement('center');
+  let t1  = document.createElement('center');
+  
+  let t2  = document.createElement('div');
+  let t3  = document.createElement('div');
+  let t4  = document.createElement('div');
+  
+  let t5  = document.createElement('span');
+  let t6  = document.createElement('span');
+  let t7  = document.createElement('span');
+  let t8  = document.createElement('span');
+  let t9  = document.createElement('span');
+  let t10 = document.createElement('span');
+  let t11 = document.createElement('span');
+  let t12 = document.createElement('span');
+  let t13 = document.createElement('span');
+  
   t1.textContent = t0;
   
-  let t2 = document.createElement('div');
   t2.textContent = t1;
+  t3.textContent = t1;
+  t4.textContent = t1; 
   
-  let t5 = document.createElement('span'):
   t5.textContent = t2;
-
-  let t6 = document.createElement('span'):
   t6.textContent = t2;
-
-  let t7 = document.createElement('span'):
   t7.textContent = t2;
 
-  let t3 = document.createElement('div');
-  t3.textContent = t1;
-    
-  let t4 = document.createElement('div');
-  t4.textContent = t1;
+  t8.textContent = t3;
+  t9.textContent = t3;
+  t10.textContent = t3;
   
+  t11.textContent = t4;
+  t12.textContent = t4;
+  t13.textContent = t4;  
+    
   t2.appendChild(t5);
   t2.appendChild(t6);
-  t2.appendChild(t7);  
+  t2.appendChild(t7);
+  
+  t3.appendChild(t8);
+  t3.appendChild(t9);
+  t3.appendChild(t10);
+  
+  t4.appendChild(t11);
+  t4.appendChild(t12);
+  t4.appendChild(t13);    
   
   t1.appendChild(t2);
-
   t1.appendChild(t3);
   t1.appendChild(t4);  
   
