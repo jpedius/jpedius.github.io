99b2b85bcba958ccfb9add788738489d5ed224ce
JP McDowell <jpedius@users.noreply.github.com>
2022-11-29 16:54:47 -0800

    Commit me

--- games/main.js
+++ games/main.js
@@ -37,7 +37,16 @@
   t1.textContent = t0;
   
   let t2 = document.createElement('div');
-  t2.textContent = t1;  
+  t2.textContent = t1;
+  
+  let t5 = document.createElement('span'):
+  t5.textContenr = t2;
+
+  let t6 = document.createElement('span'):
+  t6.textContenr = t2;
+
+  let t7 = document.createElement('span'):
+  t7.textContenr = t2;
 
   let t3 = document.createElement('div');
   t3.textContent = t1;
@@ -45,7 +54,12 @@
   let t4 = document.createElement('div');
   t4.textContent = t1;
   
+  t2.appendChild(t5);
+  t2.appendChild(t6);
+  t2.appendChild(t7);  
+  
   t1.appendChild(t2);
+
   t1.appendChild(t3);
   t1.appendChild(t4);  
   
