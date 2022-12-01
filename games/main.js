a7231478bc2b0580d0edde2d3c06b5f6ce38dc67
JP McDowell <jpedius@users.noreply.github.com>
2022-11-30 22:31:55 -0800

    Commit me

--- games/main.js
+++ games/main.js
@@ -25,6 +25,47 @@
     ]
   }
 
+  document.addEventListener('click', event => {
+
+    const target = event.target
+    const isCell = target.classList.contains('grid-cell')
+    const isDisabled = target.classList.contains('disabled')
+
+    if (isCell && !isDisabled) {
+  
+      const cellValue = target.dataset.value
+
+      game.xTurn === true
+        ? game.xState.push(cellValue)
+        : game.oState.push(cellValue)
+
+      target.classList.add('disabled')
+      target.classList.add(game.xTurn ? 'x' : 'o')
+
+      game.xTurn = !game.xTurn
+
+      // If all cells are disabled, then its draw
+      if (!document.querySelectorAll('.grid-cell:not(.disabled)').length) {
+        document.querySelector('.game-over').classList.add('visible')
+        document.querySelector('.game-over-text').textContent = 'Draw!'
+      }
+
+      game.winningStates.forEach(winningState => {
+  
+        const xWins = winningState.every(state => game.xState.includes(state))
+        const oWins = winningState.every(state => game.oState.includes(state))
+
+        if (xWins || oWins) {
+          document.querySelectorAll('.grid-cell').forEach(cell => cell.classList.add('disabled'))
+          document.querySelector('.game-over').classList.add('visible')
+          document.querySelector('.game-over-text').textContent = xWins
+            ? 'X wins!'
+            : 'O wins!'
+        }
+      })
+    }
+  })
+
   document.querySelector('.restart').addEventListener('click', () => {
     
     document.querySelector('.game-over').classList.remove('visible')
