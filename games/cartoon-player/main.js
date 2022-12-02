38badbe6fa9d64b316785b273399a2c5bced369f
JP McDowell <jpedius@users.noreply.github.com>
2022-12-01 20:52:22 -0800

    Commit me

--- games/cartoon-player/main.js
+++ games/cartoon-player/main.js
@@ -1,29 +1,21 @@
 // Game index.html
 let game = document.querySelector('#game');
+let svg = d3.select("#root");
 
-let width = 800;
-let height = 600;
+// Create a scale: transform value in pixel
+var x = d3.scaleLinear()
+    .domain([0, 100])         // This is the min and the max of the data: 0 to 100 if percentages
+    .range([0, 400]);       // This is the corresponding value I want in Pixel
+// Try console.log( x(25) ) to see what this x function does.
 
-let canvas = SVG('drawing').size('100%', '100%');
-let rect = canvas.rect(5, 5);
-let path = canvas.path("M 340,178 104,478 580,490 Z");
-let length = path.length();
-  
-path.fill('none').stroke({
-  width: 1,
-  color: '#ccc'
-}).move(10,10).scale(0.5);
+// Add 3 dots for 0, 50 and 100%
+svg.append("circle")
+  .attr("cx", x(10)).attr("cy", 100).attr("r", 40).style("fill", "blue");
+svg.append("circle")
+  .attr("cx", x(50)).attr("cy", 100).attr("r", 40).style("fill", "red");
+svg.append("circle")
+  .attr("cx", x(100)).attr("cy", 100).attr("r", 40).style("fill", "green");
 
-path.animate(3000).rotate(365).loop();
-
-rect.animate(5000, '<>').during(function(pos, morph, eased) {
-    
-    let m = path.matrixify();
-    let p = new SVG.Point(path.pointAt(eased * length)).transform(m);
-    
-    rect.move(p.x, p.y);
- 
-}).loop(true, true);
 
 /*
 let h1 = document.createElement('h1');
@@ -38,11 +30,11 @@ grass.y = 400;
 */
 
 // <svg width="200" height="200" viewBox="-100 -100 200 200">
-let root = document.createElement('svg');
-root.width = 800;
-root.height = 600;
+//let root = document.createElement('svg');
+//root.width = 800;
+//root.height = 600;
 
-console.log(root);
+//console.log(root);
 
 /*
 <circle
@@ -54,14 +46,15 @@ console.log(root);
         stroke-width="7"
       />
 */ 
+/*
 let c = document.createElement('circle');
 c.cx = 0;
 c.cy = 0;
 c.r = 90;
 c.fill = 'transparent';
 c.stroke = '#f0f0c9';
-
-root.appendChild(c);
+*/
+//root.appendChild(c);
 
 // game.appendChild(h1);
-game.appendChild(root);
+//game.appendChild(root);
