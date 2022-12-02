aabe9fd0c9b2dba89e7786c4a5864be52b108a73
JP McDowell <jpedius@users.noreply.github.com>
2022-12-01 20:22:41 -0800

    Commit me

--- games/cartoon-player/main.js
+++ games/cartoon-player/main.js
@@ -1,6 +1,30 @@
 // Game index.html
 let game = document.querySelector('#game');
 
+let width = 800;
+let height = 600;
+
+let canvas = SVG('drawing').size('100%', '100%');
+let rect = canvas.rect(5, 5);
+let path = canvas.path("M 340,178 104,478 580,490 Z");
+let length = path.length();
+  
+path.fill('none').stroke({
+  width: 1,
+  color: '#ccc'
+}).move(10,10).scale(0.5);
+
+path.animate(3000).rotate(365).loop();
+
+rect.animate(5000, '<>').during(function(pos, morph, eased) {
+    
+    let m = path.matrixify();
+    let p = new SVG.Point(path.pointAt(eased * length)).transform(m);
+    
+    rect.move(p.x, p.y);
+ 
+}).loop(true, true);
+
 /*
 let h1 = document.createElement('h1');
 h1.innerHTML = 'JP';
