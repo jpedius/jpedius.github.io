e0951f083ac939bed52a71f748883f7d98ede93c
JP McDowell <jpedius@users.noreply.github.com>
2022-12-01 16:45:20 -0800

    Commit me

--- games/cartoon-player/main.js
+++ games/cartoon-player/main.js
@@ -1,6 +1,11 @@
+// Game index.html
 let game = document.querySelector('#game');
-let h1 = document.createElement('h1');
 
+let h1 = document.createElement('h1');
 h1.innerHTML = 'JP';
 
+let grass = document.createElement('img');
+grass.src = 'grass.webp';
+
 game.appendChild(h1);
+game.appendChild(grass);
