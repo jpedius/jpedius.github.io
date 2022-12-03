4fe705d4104a836190f803d4fd2a4db27ac74384
JP McDowell <jpedius@users.noreply.github.com>
2022-12-02 19:18:22 -0800

    Commit me

--- games/cartoon-player/main.js
+++ games/cartoon-player/main.js
@@ -108,7 +108,7 @@ class Ball {
 
 const balls = [];
 
-while (balls.length < 25) {
+while (balls.length < 2) {
    const size = random(10,20);
    const ball = new Ball(
       // ball position always drawn at least one ball width
