9021b48d7153190907a13e6612744f9d528617af
JP McDowell <jpedius@users.noreply.github.com>
2022-12-02 18:47:31 -0800

    Commit me

--- games/cartoon-player/main.js
+++ games/cartoon-player/main.js
@@ -9,6 +9,17 @@ const ctx = canvas.getContext('2d');
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
+// This will log the width of the viewport
+console.log(window.innerWidth);
+
+// This will log the width of the frame viewport within a frameset
+console.log(self.innerWidth);
+
+// This will log the width of the viewport of the closest frameset
+console.log(parent.innerWidth);
+
+// This will log the width of the viewport of the outermost frameset
+console.log(top.innerWidth);
 
 /*
 //let svg = d3.select("#root");
