ba3d3d651e819459a1e29f8636a3c053d42be6fc
JP McDowell <jpedius@users.noreply.github.com>
2022-12-04 19:34:22 -0800

    Commit me

--- tutorial/individual-words/main.js
+++ tutorial/individual-words/main.js
@@ -6,7 +6,7 @@
     let src = "/tutorial/storage/plot/" + letSelectPlot.value + ".txt";
     $.get(src, function(data) {
       previousOrNext = 0;     
-      //howMany = data.match( /[^\.!\?]+[\.!\?]+/g ).map(x => x.trim());
+      howMany = data.match( /[^\.!\?]+[\.!\?]+/g ).map(x => x.trim());
       console.log(data);
       //howMany = data.split(' ');
       console.log(howMany);
