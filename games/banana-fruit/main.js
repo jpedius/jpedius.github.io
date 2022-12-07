5b8574506003d89ea33fe55861d288899f4c287d
JP McDowell <jpedius@users.noreply.github.com>
2022-12-06 19:59:03 -0800

    Commit me

--- games/banana-fruit/main.js
+++ games/banana-fruit/main.js
@@ -3,7 +3,9 @@
 (function($) {
 
     let game = document.querySelector('.game');
+    game.innerHTML = 'The boy took his dog for a run in the park because his mother is cleaning the house';
     
     console.log(game);
+    console.log(game.textContent, game.innerText, game.innerHTML);
  
 })(jQuery);
