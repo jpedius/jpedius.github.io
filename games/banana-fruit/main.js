0a781df755082adaac23f86078b8e137f77482da
JP McDowell <jpedius@users.noreply.github.com>
2022-12-06 21:15:58 -0800

    Commit me

--- games/banana-fruit/main.js
+++ games/banana-fruit/main.js
@@ -5,7 +5,7 @@
     let game = document.querySelector('.game');
     let a = 'The boy took his dog for a run in the park' +
         ' because his mother is cleaning the house' +
-        ' <span>Hello World</span>';
+        ' <em>Hello World</em>';
     
     //game.innerHTML = a;
     //console.log(game.innerHTML);
