5a5e4eeaaea207e2710a8574c6a72c878a412d5e
JP McDowell <jpedius@users.noreply.github.com>
2022-12-06 23:49:47 -0800

    Commit me

--- games/banana-fruit/main.js
+++ games/banana-fruit/main.js
@@ -6,8 +6,12 @@
     let a = '<p>The boy took his dog for a run in the park' +
         ' because his mother is cleaning the house' +
         ' <em>Hello World</em></p>';
-    
     game.innerHTML = a;
+    
+    let nav = document.createElement('nav');
+    nav.innerHTML = '<p>navigation</p>';
+    game.appendChild(nav);
+    
 /*    
     var x1 = document.getElementById("example1").innerText;
     console.log(x1);
