0ae671970e9f511929010c46e7dbfd88d95bde8f
JP McDowell <jpedius@users.noreply.github.com>
2022-12-06 20:31:58 -0800

    Commit me

--- games/banana-fruit/main.js
+++ games/banana-fruit/main.js
@@ -3,7 +3,18 @@
 (function($) {
 
     let game = document.querySelector('.game');
-    game.innerHTML = 'The boy took his dog for a run in the park because his mother is cleaning the house';
+    let a = 'The boy took his dog for a run in the park' +
+        ' because his mother is cleaning the house' +
+        ' <span>Hello World</span>';
+    
+    game.innerHTML(a);
+    console.log(game.innerHTML);
+   
+    game.innerText(a);
+    console.log(game.innerText);
+    
+    game.textContent(a);
+    console.log(game.textContent);
     
     console.log(game);
     console.log(game.textContent, game.innerText, game.innerHTML);
