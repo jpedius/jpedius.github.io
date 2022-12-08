8ff56ee7e5f536bed11aeebfae2031f187237048
JP McDowell <jpedius@users.noreply.github.com>
2022-12-07 23:39:33 -0800

    Commit me

--- tutorial/whole-sentences-two/main.js
+++ tutorial/whole-sentences-two/main.js
@@ -22,7 +22,37 @@
     let play = document.createElement('button');
     play.innerHTML = 'Play';
     root.appendChild(play);
-
+    
+    let forRate = document.createElement('label');
+    forRate.for = 'rate';
+    forRate.innerHTML = 'Rate';
+    root.appendChild(forRate);
+    
+    let rate = document.createElement('input');
+    rate.type = 'range';
+    rate.min = 0.5;
+    rate.max = 2;
+    rate.value = '1';
+    rate.step = 0.1;
+    root.appendChild(rate);
+    
+    let forPitch = document.createElement('label');
+    forPitch.for = 'pitch';
+    forPitch.innerHTML = 'Pitch';
+    root.appendChild(forPitch);
+    
+    let pitch = document.createElement('input');
+    pitch.type = 'range';
+    pitch.min = 0;
+    pitch.max = 2;
+    pitch.value = '1';
+    pitch.step = 0.1;
+    root.appendChild(pitch);
+    
+    let voice = document.createElement('select');
+    root.appendChild(voice);
+      
     console.log(title, root, h1, previous, next, play);
+    console.log(forRate, rate, forPitch, pitch, voice);
 
 })(jQuery);
