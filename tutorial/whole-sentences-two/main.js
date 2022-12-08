e883896789888ee5c653c93207187e58943d41cc
JP McDowell <jpedius@users.noreply.github.com>
2022-12-07 23:15:38 -0800

    Commit me

--- tutorial/whole-sentences-two/main.js
+++ tutorial/whole-sentences-two/main.js
@@ -10,7 +10,19 @@
     let h1 = document.createElement('h1');
     h1.innerHTML = title;
     root.appendChild(h1);
+    
+    let previous = document.createElement('button');
+    previous.innerHTML = 'Previous';
+    root.appendChild(previous);
+    
+    let next = document.createElement('button');
+    next.innerHTML = 'Next';
+    root.appendChild(next);
+    
+    let play = document.createElement('button');
+    play.innerHTML = 'Play';
+    root.appendChild(play);
 
-    console.log(title, root, h1);
+    console.log(title, root, h1, previous, next, play);
 
 })(jQuery);
