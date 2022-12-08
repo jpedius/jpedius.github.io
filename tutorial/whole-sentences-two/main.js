ab8a89a66919664e2b0f3926204ed080f03fa651
JP McDowell <jpedius@users.noreply.github.com>
2022-12-08 12:20:33 -0800

    Commit me

--- tutorial/whole-sentences-two/main.js
+++ tutorial/whole-sentences-two/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import { voice } from "/tutorial/whole-sentences/speech.js";
+
 (function($) {
 
     let title = 'Whole Sentences';
@@ -66,101 +68,12 @@
     root.appendChild(divRatePitch);
     
     let divVoice = document.createElement('div');
-    
-    let voice = document.createElement('select');
+
     divVoice.appendChild(voice);
     
     root.appendChild(divVoice);
    
     console.log(title, root, h1, previous, next, play);
     console.log(forRate, rate, forPitch, pitch, voice);
-
-    // "Microsoft Zira - English (United States)"
-    // "Samantha"
-    // "Daniel"
-
-    const synth = window.speechSynthesis;
-    let voices = [];
-
-    function populateVoiceList() {
-
-        // Sorted by
-        voices = synth.getVoices().sort(function (a, b) {
-            const aname = a.name.toUpperCase();
-            const bname = b.name.toUpperCase();
-            if (aname < bname) {
-                return -1;
-            } else if (aname == bname) {
-                return 0;
-            } else {
-                return +1;
-            }
-        });
-
-        voice.innerHTML = "";
-        
-        for (let i = 0; i < voices.length; i++) {
-            
-            if (voices[i].lang.slice(0, 3) === "en-") {
-                
-                const option = document.createElement("option");
-                option.textContent = `${voices[i].name} (${voices[i].lang})`;
-                option.setAttribute("data-lang", voices[i].lang);
-                option.setAttribute("data-name", voices[i].name);
-                
-                if (voices[i].name === "Microsoft Zira - English (United States)") {
-                    option.defaultSelected = true;
-                }
-                else if (voices[i].name === "Samantha") {
-                    option.defaultSelected = true;
-                }
-                else if (voices[i].name === "Tessa") {
-                    option.defaultSelected = true;
-                }
-                
-                voice.appendChild(option);
-            }
-        }
-    }
-
-    populateVoiceList();
-    if (speechSynthesis.onvoiceschanged !== undefined) {
-        speechSynthesis.onvoiceschanged = populateVoiceList;
-    }
-  
-    function speak(talk) {
-
-        if (synth.speaking) {
-            console.error("speechSynthesis.speaking");
-            return;
-        }
-
-        if (voice !== '') {
-            
-            const utterThis = new SpeechSynthesisUtterance(talk);
-            
-            utterThis.onend = function (event) {
-                console.log("SpeechSynthesisUtterance.onend");
-            };
-
-            utterThis.onerror = function (event) {
-                console.error("SpeechSynthesisUtterance.onerror");
-            };
-
-            const selectedOption =
-                voiceSelect.selectedOptions[0].getAttribute("data-name");
-
-            for (let i = 0; i < voices.length; i++) {
-                if (voices[i].name === selectedOption) {
-                    utterThis.voice = voices[i];
-                    break;
-                }
-            }
-
-            utterThis.pitch = pitch.value;
-            utterThis.rate = rate.value;
-            synth.speak(utterThis);
-        }
-    }
 
 })(jQuery);
