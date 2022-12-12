75fe5648fa66b60c7431bae9c9224d1ed6dd7615
JP McDowell <jpedius@users.noreply.github.com>
2022-12-11 18:32:04 -0800

    Commit me

--- tutorial/audio-sounds/main.js
+++ tutorial/audio-sounds/main.js
@@ -1,51 +1,178 @@
+'use strict';
+
 jQuery(function($) {
-  'use strict';
 
-  const consonant = [
-    { letter: "p", mp3: "../storage/sounds/consonant-p.mp3", },
-    { letter: "b", mp3: "../storage/sounds/consonant-b.mp3", },
-    { letter: "f", mp3: "../storage/sounds/consonant-f.mp3", },
-    { letter: "v", mp3: "../storage/sounds/consonant-v.mp3", },
-    { letter: "t", mp3: "../storage/sounds/consonant-t.mp3", },
-    { letter: "d", mp3: "../storage/sounds/consonant-d.mp3", },
-    { letter: "c/k", mp3: "../storage/sounds/consonant-k.mp3", },
-    { letter: "g", mp3: "../storage/sounds/consonant-g.mp3", },
-    { letter: "th", mp3: "../storage/sounds/consonant-th-quiet.mp3", },
-    { letter: "TH", mp3: "../storage/sounds/consonant-th-noisy.mp3", },
-    { letter: "s", mp3: "../storage/sounds/consonant-s.mp3", },
-    { letter: "z", mp3: "../storage/sounds/consonant-z.mp3", },
-    { letter: "sh", mp3: "../storage/sounds/consonant-sh.mp3", },
-    { letter: "su/zh/zu", mp3: "../storage/sounds/consonant-zh.mp3", },
-    { letter: "ch", mp3: "../storage/sounds/consonant-ch.mp3", },
-    { letter: "j", mp3: "../storage/sounds/consonant-j.mp3", },
-    { letter: "m", mp3: "../storage/sounds/consonant-m.mp3", },
-    { letter: "n", mp3: "../storage/sounds/consonant-n.mp3", },
-    { letter: "ng", mp3: "../storage/sounds/consonant-ng.mp3", },
-    { letter: "l", mp3: "../storage/sounds/consonant-l.mp3", },
-    { letter: "r", mp3: "../storage/sounds/consonant-r.mp3", },
-    { letter: "h", mp3: "../storage/sounds/consonant-h.mp3", },
-    { letter: "w", mp3: "../storage/sounds/consonant-w.mp3", },
-  ];
+    const file = '/tutorial/audio-sounds/file/';
+    
+    const consonant = [{ 
+        letter: "p",
+        mp3: "consonant-p.mp3",
+    }, { 
+        letter: "b", 
+        mp3: "consonant-b.mp3",
+    }, { 
+        letter: "f", 
+        mp3: "consonant-f.mp3",
+    }, { 
+        letter: "v", 
+        mp3: "consonant-v.mp3",
+    }, { 
+        letter: "t", 
+        mp3: "consonant-t.mp3",
+    }, { 
+        letter: "d", 
+        mp3: "consonant-d.mp3",
+    }, { 
+        letter: "c/k", 
+        mp3: "consonant-k.mp3",
+    }, { 
+        letter: "g", 
+        mp3: "consonant-g.mp3",
+    }, { 
+        letter: "th", 
+        mp3: "consonant-th-quiet.mp3",
+    }, { 
+        letter: "TH", 
+        mp3: "consonant-th-noisy.mp3",
+    }, { 
+        letter: "s", 
+        mp3: "consonant-s.mp3",
+    }, { 
+        letter: "z", 
+        mp3: "consonant-z.mp3",
+    }, { 
+        letter: "sh", 
+        mp3: "consonant-sh.mp3",
+    }, { 
+        letter: "su/zh/zu", 
+        mp3: "consonant-zh.mp3",
+    }, { 
+        letter: "ch", 
+        mp3: "consonant-ch.mp3",
+    }, { 
+        letter: "j", 
+        mp3: "consonant-j.mp3",
+    }, { 
+        letter: "m", 
+        mp3: "consonant-m.mp3",
+    }, { 
+        letter: "n", 
+        mp3: "consonant-n.mp3",
+    }, { 
+        letter: "ng", 
+        mp3: "consonant-ng.mp3",
+    }, { 
+        letter: "l", 
+        mp3: "consonant-l.mp3",
+    }, { 
+        letter: "r", 
+        mp3: "consonant-r.mp3",
+    }, { 
+        letter: "h", 
+        mp3: "consonant-h.mp3",
+    }, { 
+        letter: "w", 
+        mp3: "consonant-w.mp3",
+    }];
 
-  const vowel = [
-    { letter: "ee", mp3: "../storage/sounds/vowel-ee.mp3", },
-    { letter: "i", mp3: "../storage/sounds/vowel-i.mp3", },
-    { letter: "e", mp3: "../storage/sounds/vowel-e.mp3", },
-    { letter: "ae", mp3: "../storage/sounds/vowel-ae.mp3", },
-    { letter: "a", mp3: "../storage/sounds/vowel-a.mp3", },
-    { letter: "u", mp3: "../storage/sounds/vowel-u.mp3", },
-    { letter: "au/aw/o", mp3: "../storage/sounds/vowel-au-aw-o.mp3", },
-    { letter: "oe", mp3: "../storage/sounds/vowel-oe.mp3", },
-    { letter: "uu", mp3: "../storage/sounds/vowel-uu.mp3", },
-    { letter: "oo", mp3: "../storage/sounds/vowel-oo.mp3", },
-    { letter: "ie", mp3: "../storage/sounds/vowel-ie.mp3", },
-    { letter: "ue", mp3: "../storage/sounds/vowel-ue.mp3", },
-    { letter: "ou/ow", mp3: "../storage/sounds/vowel-ou-ow.mp3", },
-    { letter: "oi/oy", mp3: "../storage/sounds/vowel-oi-oy.mp3", },
-    { letter: "er/ir/ur", mp3: "../storage/sounds/vowel-er-ir-ur.mp3", },
-    { letter: "ar", mp3: "../storage/sounds/vowel-ar.mp3", },
-    { letter: "or", mp3: "../storage/sounds/vowel-or.mp3", },
-  ];
+    const vowel = [{ 
+        letter: "ee", 
+        mp3: "vowel-ee.mp3",
+    }, { 
+        letter: "i", 
+        mp3: "vowel-i.mp3",
+    }, { 
+        letter: "e", 
+        mp3: "vowel-e.mp3",
+    }, { 
+        letter: "ae", 
+        mp3: "vowel-ae.mp3",
+    }, { 
+        letter: "a", 
+        mp3: "vowel-a.mp3",
+    }, { 
+        letter: "u", 
+        mp3: "vowel-u.mp3",
+    }, { 
+        letter: "au/aw/o", 
+        mp3: "vowel-au-aw-o.mp3",
+    }, { 
+        letter: "oe", 
+        mp3: "vowel-oe.mp3",
+    }, { 
+        letter: "uu", 
+        mp3: "vowel-uu.mp3",
+    }, { 
+        letter: "oo", 
+        mp3: "vowel-oo.mp3",
+    }, { 
+        letter: "ie", 
+        mp3: "vowel-ie.mp3",
+    }, { 
+        letter: "ue", 
+        mp3: "vowel-ue.mp3",
+    }, { 
+        letter: "ou/ow", 
+        mp3: "vowel-ou-ow.mp3",
+    }, { 
+        letter: "oi/oy", 
+        mp3: "vowel-oi-oy.mp3",
+    }, { 
+        letter: "er/ir/ur", 
+        mp3: "vowel-er-ir-ur.mp3",
+    }, { 
+        letter: "ar", 
+        mp3: "vowel-ar.mp3",
+    }, { 
+        letter: "or", 
+        mp3: "vowel-or.mp3",
+    }];
+
+    const sounds = consonant + vowel;
+
+    let root = document.querySelector('#root');
+    root.classList.add('q11');
+
+    let h1Title = document.createElement('h1');
+    h1Title.innerHTML = title;
+    h1Title.classList.add('q12');
+    root.appendChild(h1Title);
+  
+    for (let i=0; i<sounds.length; i++) {
+    
+        let divSounds = document.createElement('div');
+        divSounds.classList.add('q19');
+
+        let toAudio = document.createElement('audio');
+        toAudio.classList.add('q19');
+        toAudio.src = file + sounds[i].mp3
+        divSounds.appendChild(toAudio);
+
+        let toButton = document.createElement('button');
+        toButton.classList.add('q19');
+        toButton.type = 'button';
+        let a = 0;
+        toButton.addEventListener('click', () => {
+            divSounds.children[a].play();
+        });
+        divSounds.appendChild(toButton);
+
+        let toInput = document.createElement('input');
+        toInput.classList.add('q19');
+        toInput.type = 'text';
+        toInput.size = 10;
+        divSounds.appendChild(toInput);
+
+        let toSpan = document.createElement('span');
+        toSpan.classList.add('q19');
+        toSpan.innerHTML = ' ' + sounds[i].letter + ' ';
+        divSounds.appendChild(toSpan);
+
+        root.appendChild(divSounds);
+    } 
+
+/*
+
 
   function shuffle(array) {
 
@@ -144,4 +271,6 @@ jQuery(function($) {
   };
 
   main();
+  
+*/ 
 });
