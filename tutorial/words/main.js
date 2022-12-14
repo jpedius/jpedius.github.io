f566aef9abcbf1487950aa33bdfa5c2277260f47
JP McDowell <jpedius@users.noreply.github.com>
2022-12-13 23:55:06 -0800

    Commit me

--- tutorial/words/main.js
+++ tutorial/words/main.js
@@ -1,24 +1,6 @@
 'use strict';
 
 (function($) {
-/*
-    let special = [
-        'zeroth', 'first', 'second', 'third', 'fourth', 'fifth',
-        'sixth', 'seventh', 'eighth', 'ninth', 'tenth', 'eleventh',
-        'twelfth', 'thirteenth', 'fourteenth', 'fifteenth',
-        'sixteenth', 'seventeenth', 'eighteenth', 'nineteenth'
-    ];
-    let deca = [
-        'twent', 'thirt', 'fort', 'fift', 'sixt', 'sevent',
-        'eight', 'ninet'
-    ];
-
-    function stringifyNumber(n) {
-        if (n < 20) return special[n];
-        if (n%10 === 0) return deca[Math.floor(n/10)-2] + 'ieth';
-        return deca[Math.floor(n/10)-2] + 'y-' + special[n%10];
-    }
-*/
 
     let title = 'Words';
     document.title = title;
