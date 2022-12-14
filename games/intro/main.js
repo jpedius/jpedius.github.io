8581dce976ed405a3231ed79d263f8deca5f5977
JP McDowell <jpedius@users.noreply.github.com>
2022-12-14 15:57:51 -0800

    Commit me

--- games/intro/main.js
+++ games/intro/main.js
@@ -12,14 +12,27 @@
     h1Title.innerHTML = title;
     root.appendChild(h1Title);
  
-    window.addEventListener('load', (event) => {
-        startGame();
-    });
+    window.addEventListener('load', (event) => { startGame() });
+    
+    let myGamePiece;
     
     function startGame() {
         myGameArea.start();
+        myGamePiece = new component(30, 30, 'red', 10, 120);
     }
     
+    function component(width, height, color, x, y) {
+    
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+        
+        let ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x this.y, this.width, this.height);
+    }
+     
     let myGameArea = {
         canvas : document.createElement("canvas"),
         start : function() {
