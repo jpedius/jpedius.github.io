0541d41ef0064707972b103d1247d70358d8ceab
JP McDowell <jpedius@users.noreply.github.com>
2022-12-13 16:56:40 -0800

    Commit me

--- tutorial/write-words/main.js
+++ tutorial/write-words/main.js
@@ -1,91 +1,259 @@
 'use strict';
 
-let previousOrNext = 0;
-let howMany = read_sentences(words);
-howMany = shuffle(howMany);
+(function($) {
 
-let inputBinary = false;
-const inputTxt = document.querySelector(".txt");
-inputTxt.value = howMany[previousOrNext];
+    let title = 'Words';
+    document.title = title;
 
-const one = document.querySelector("#one");
+    let readonly = document.createElement('input');
+    readonly.classList.add('q02a');
+    readonly.type = 'text';
+    readonly.readonly = 'readonly';
+    readonly.autocomplete = 'off';
+    readonly.autocorrect = 'off';
+    readonly.autocapitalize = 'off';
+    
+    let text = document.createElement('input');
+    text.classList.add('q02');
+    text.type = 'text';
+    text.autocomplete = 'off';
+    text.autocorrect = 'off';
+    text.autocapitalize = 'off';
+    
+    let previousOrNext = 0;
+    let howMany = [];
+    for (const i of words) {
+        howMany.push(i.word);
+    }
+    howMany = shuffle(howMany);
+    setText();
 
-//console.log('words', words.length, words[0].length, words[0][0].length);
+    function setText() {
+        let b = howMany[previousOrNext];
+        let a = '';
+        for (let i=0; i<b.length; i++) {
+            a += b[i] + ' ';
+        }
+        readonly.value = a.trim();
+        text.value = '';
+    }
 
-//console.log('howMany', howMany.length, howMany[0]);
+    let previous = document.createElement('button');
+    previous.innerHTML = 'Previous';
+    previous.classList.add('q03');
+    previous.addEventListener('click', function() {
+        if (previousOrNext <= 0) {
+            previousOrNext = howMany.length;
+        }
+        previousOrNext--;
+        setText();
+    }, false);
+    
+    let play = document.createElement('button');
+    play.innerHTML = 'Play';
+    play.classList.add('q04');
+    play.addEventListener('click', function() { 
+        if (text.value !== '') {
+            speak(readonly.value);
+            text.blur();
+        }
+    }, false);
 
-const counts = { num: 'num' };
+    let next = document.createElement('button');
+    next.innerHTML = 'Next';
+    next.classList.add('q05');
+    next.addEventListener('click', function() {
+        if (previousOrNext >= howMany.length - 1) {
+            previousOrNext = -1;
+        }
+        previousOrNext++;
+        setText();
+    }, false);
 
-for (const num of howMany) {
-  let num2 = num.length;
-  counts[num2] = counts[num2] ? counts[num2] + 1 : 1;
-}
+    let forRate = document.createElement('label');
+    forRate.htmlFor = 'rate';
+    forRate.innerHTML = 'Rate';
+    forRate.classList.add('q06');
 
-console.log('counts', counts, counts.length);
-/*
-for (let i=0; i<howMany.length; i++) {
-  console.log('i', i);
-}
-*/
-/*
-const n = howMany; //["c80", "b9", "d700", "a8", "a543"];
+    let rate = document.createElement('input');
+    rate.type = 'range';
+    rate.min = 0.5;
+    rate.max = 2;
+    rate.defaultValue = '1';
+    rate.step = 0.1;
+    rate.classList.add('q07');
 
-let h = n.sort((a, b) => {
-  if (a.length < b.length) { return -1; }
-  else if (a.length > b.length) { return 1; }
-  else if (a < b) { return -1; }
-  else if (a > b) { return 1; }
-  return 0;
-});
+    let forPitch = document.createElement('label');
+    forPitch.htmlFor = 'pitch';
+    forPitch.innerHTML = 'Pitch';
+    forPitch.classList.add('q08');
 
-console.log('h 02', h);
-*/
-//for (let i=0; i<h.length; i++) {
-//  console.log("{ 'word': '" + h[i] + "' },");
-//}
+    let pitch = document.createElement('input');
+    pitch.type = 'range';
+    pitch.min = 0;
+    pitch.max = 2;
+    pitch.defaultValue = '1';
+    pitch.step = 0.1;
+    pitch.classList.add('q09');
 
-function setText() {
-  one.value = "";
-  return inputTxt.value = howMany[previousOrNext];
-}
+    // "Microsoft Zira - English (United States)"
+    // "Samantha"
+    // "Daniel"
+    // "Tessa"
 
-function show() {
-  if (inputBinary === false) {
-    inputTxt.value = '-----';
-    inputBinary = true; 
-  }
-  else {
-    inputTxt.value = howMany[previousOrNext];
-    inputBinary = false;
-  }
-}
+    let voice = document.createElement('select');
+    voice.classList.add('q10');
 
-function play() {
-  speak(howMany[previousOrNext]);
-  inputTxt.blur();
-}
+    const synth = window.speechSynthesis;
+    let voices = [];
 
-function read_sentences(rs) {
-  let items = [];
-  for (const i of rs) {
-      items.push(i.word);
-  }
-  console.log('items', items.length);
-  return items;
-};
+    function populateVoiceList() {
 
-function previous() {
-  if (previousOrNext <= 0) {
-    previousOrNext = howMany.length;
-  }
-  previousOrNext--;
-  return setText();
-}
+        // Sorted by
+        voices = synth.getVoices().sort(function (a, b) {
+            const aname = a.name.toUpperCase();
+            const bname = b.name.toUpperCase();
+            if (aname < bname) {
+                return -1;
+            } else if (aname == bname) {
+                return 0;
+            } else {
+                return +1;
+            }
+        });
 
-function next() {
-  if (previousOrNext >= howMany.length - 1) {
-    previousOrNext = -1;
-  }
-  previousOrNext++;
-  return setText();
-}
+        voice.innerHTML = "";
+
+        for (let i = 0; i < voices.length; i++) {
+
+            if (voices[i].lang.slice(0, 3) === "en-") {
+
+                const option = document.createElement("option");
+                option.textContent = `${voices[i].name} (${voices[i].lang})`;
+                option.setAttribute("data-lang", voices[i].lang);
+                option.setAttribute("data-name", voices[i].name);
+
+                if (voices[i].name === "Microsoft Zira - English (United States)") {
+                    option.defaultSelected = true;
+                }
+                else if (voices[i].name === "Samantha") {
+                    option.defaultSelected = true;
+                }
+                else if (voices[i].name === "Tessa") {
+                    option.defaultSelected = true;
+                }
+
+                voice.appendChild(option);
+            }
+        }
+    }
+
+    populateVoiceList();
+    if (speechSynthesis.onvoiceschanged !== undefined) {
+        speechSynthesis.onvoiceschanged = populateVoiceList;
+    }
+
+    function speak(talk) {
+
+        if (synth.speaking) {
+            console.error("speechSynthesis.speaking");
+            return;
+        }
+
+        if (voice !== '') {
+
+            const utterThis = new SpeechSynthesisUtterance(talk);
+
+            utterThis.onend = function (event) {
+                console.log("SpeechSynthesisUtterance.onend");
+            };
+
+            utterThis.onerror = function (event) {
+                console.error("SpeechSynthesisUtterance.onerror");
+            };
+
+            const selectedOption =
+                voice.selectedOptions[0].getAttribute("data-name");
+
+            for (let i = 0; i < voices.length; i++) {
+                if (voices[i].name === selectedOption) {
+                    utterThis.voice = voices[i];
+                    break;
+                }
+            }
+
+            utterThis.pitch = pitch.value;
+            utterThis.rate = rate.value;
+            synth.speak(utterThis);
+        }
+    }
+ 
+    function shuffle(array) {
+
+        let items = JSON.parse(JSON.stringify(array));
+        let currentIndex = items.length, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (currentIndex !== 0) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            // And swap it with the current element.
+            [items[currentIndex], items[randomIndex]] = [
+                items[randomIndex], items[currentIndex]];
+        }
+
+        return items;
+    }  
+
+    let root = document.querySelector('#root');
+    root.classList.add('q11');
+
+    let h1Title = document.createElement('h1');
+    h1Title.innerHTML = title;
+    h1Title.classList.add('q12');
+    root.appendChild(h1Title);
+
+    let divReadonly = document.createElement('div');
+    divReadonly.classList.add('q13a');
+    divReadonly.appendChild(readonly);
+    root.appendChild(divReadonly);
+    
+    let divText = document.createElement('div');
+    divText.classList.add('q13');
+    divText.appendChild(text);
+    root.appendChild(divText);
+
+    let divButton = document.createElement('div');    
+    divButton.classList.add('q14');
+    divButton.appendChild(previous);
+    divButton.appendChild(play);
+    divButton.appendChild(next);
+    root.appendChild(divButton);
+
+    let divRatePitch = document.createElement('div');
+    divRatePitch.classList.add('q15');
+
+    let spanRate = document.createElement('span');
+    spanRate.classList.add('q16');
+    spanRate.appendChild(forRate);
+    spanRate.appendChild(rate);
+    divRatePitch.appendChild(spanRate);
+
+    let spanPitch = document.createElement('span');
+    spanPitch.classList.add('q17');
+    spanPitch.appendChild(forPitch);
+    spanPitch.appendChild(pitch);
+    divRatePitch.appendChild(spanPitch); 
+
+    root.appendChild(divRatePitch);
+
+    let divVoice = document.createElement('div');
+    divVoice.classList.add('q18');
+    divVoice.appendChild(voice);
+    root.appendChild(divVoice);
+
+})(jQuery);
+
