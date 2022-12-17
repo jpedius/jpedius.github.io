263cf6fad83b9e9443b4ed2cc09d2ba2e8fbfef4
JP McDowell <jpedius@users.noreply.github.com>
2022-12-16 19:15:52 -0800

    Commit me

--- games/intro/main.js
+++ games/intro/main.js
@@ -14,6 +14,82 @@
  
     window.addEventListener('load', (event) => { game() });
     
+    class GameComponent {
+    
+        constructor(width, height, color, x, y) {
+        
+            this.width = width;
+            this.height = height;
+            this.color = color;
+            this.x = x;
+            this.y = y;
+            
+            console.log(width, height, color, x, y);
+        }
+        
+        update(g) {
+
+            let ctx = g.context;
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+            
+            console.log(ctx, g);
+        }
+    }
+    
+    class GameCanvas {
+    
+        constructor(root) {
+            this.root = root;
+            this.canvas = document.createElement("canvas");
+            
+            console.log(root, this.canvas);
+        }
+        
+        initialize() {
+
+            const a = [];
+            a[0] = new GameComponent(30, 30, 'blue',    0,   0);
+            a[1] = new GameComponent(30, 30, 'red',   110, 120);
+            a[2] = new GameComponent(10, 10, 'green', 210, 120); 
+
+            console.log(a);
+
+            return a;
+        }
+        
+        start(c) {
+
+            this.canvas.width = 800;
+            this.canvas.height = 600;
+            
+            this.context = this.canvas.getContext("2d");
+            
+            root.appendChild(this.canvas);
+            
+            this.interval = setInterval(function {
+                this.root.clear(); 
+                c.update(this.root);
+                c.update(this.root);
+                c.update(this.root);
+            }, 20);
+            
+            console.log(this.interval);
+        }
+        
+        clear() {
+            this.context.clearRect(
+                0, 0, this.canvas.width, this.canvas.height);
+                
+            console.log(this.context);
+        }   
+    }
+    
+    const game = new GameRoot(root);
+    const comp = game.initialize();
+    const star = game.start(comp)
+/*    
+    
     let myGamePiece1;
     let myGamePiece2;
     let myGamePiece3;
@@ -78,40 +154,40 @@
 
     class GameArea {
     
-        start() {
-        
-            console.log('GameArea start');
+        constructor(width, height, color, x, y) {
+
+            this.width = width;
+            this.height = height;
+            
+            this.color = color;
+            
+            this.x = x;
+            this.y = y;
         }
         
-        update() {
-        
+        update(a) {
+
+            this.update = function() { 
+                let ctx = myGameArea.context;
+                    ctx.fillStyle = this.ucolor;
+                    ctx.fillRect(this.x, this.y, this.width, this.height);
+            }
+             
             console.log('GameArea update');
         }
     }
- 
-    /*
-    function GameRoot() {
-        g.init(area.start());
-        g.start(area.update());
-    }
-    */
+    
+    const a1 = new GameComponent(30, 30, 'blue',    0,   0);
+    const a2 = new GameComponent(30, 30, 'red',   110, 120);
+    const a3 = new GameComponent(10, 10, 'green', 210, 120); 
 
     class GameCanvas {
     
         constructor(root) {
             this.root = root;
-            this.canvas = document.createElement("canvas");
-            
-            console.log(this.root, this.canvas)
+            this.canvas = document.createElement("canvas"); 
         }
 
-        init(start) {
-            
-            this.init = start;
-            
-            console.log(this.init)
-        }
-        
         start(update) {
         
             this.canvas.width = 800;
@@ -136,5 +212,5 @@
     }
     
     const g = new GameCanvas(root);
-
+*/
 })(jQuery);
