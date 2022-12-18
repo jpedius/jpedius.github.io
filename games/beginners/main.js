094b927b00bed720f436d259cf171a61abed4f5f
JP McDowell <jpedius@users.noreply.github.com>
2022-12-17 21:34:49 -0800

    Commit me

--- games/beginners/main.js
+++ games/beginners/main.js
@@ -25,14 +25,37 @@ let frameX = 0;
 let frameY = 0;
 let gameFrame = 0;
 let staggerFrames = 5;
+let spriteAnimations = [];
+let animationStates = [{
+    name: 'idle',
+    frame: 7,
+}, {
+    name: 'jump',
+    frame: 7,
+}];
+
+animationStates.forEach((state, index) => {
+    let frames = {
+        loc: [],
+    };
+    for (let j<0; state.frames; j++) {
+        let positionX = j * spriteWidth;
+        let positionY = index * spriteHeight;
+        frames.loc.push({x: positionX, y: positionY});
+    }
+    spriteAnimations[state.name] = frames;
+});
+console.log(spriteAnimations);
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     //ctx.fillRect(50, 50, 100, 100);
+    let position = Math.floor(gameFrame / staggerFramas) % 6;
+    frameX = spriteImage * position;
     ctx.drawImage(playerImage,
-        frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight,
+        frameX, frameY * spriteHeight, spriteWidth, spriteHeight,
         0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    
+    /*
     if (gameFrame % staggerFrames == 0) {
         if (frameX < 6) {
             frameX++;
@@ -40,6 +63,7 @@ function animate() {
             frameX = 0;
         }
     }
+    */
     gameFrame++;
  
     requestAnimationFrame(animate);
