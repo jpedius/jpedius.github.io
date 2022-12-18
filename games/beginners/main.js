5c4626fe8b0e8e8081a08db582758990cd5edfe3
JP McDowell <jpedius@users.noreply.github.com>
2022-12-17 16:15:07 -0800

    Commit me

--- games/beginners/main.js
+++ games/beginners/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let title = 'Intro';
+let title = 'Pixel Animation';
 document.title = title;
 
 let root = document.querySelector('#root');
@@ -8,5 +8,18 @@ let root = document.querySelector('#root');
 let canvas = document.createElement('canvas');
 let ctx = canvas.getContext('2d');
 console.log(ctx);
+
+let CANVAS_WIDTH = canvas.width = 600;
+let CANVAS_HEIGHT = canvas.height = 600;
+
+let playerImage = new Image();
+playerImage.src = 'shadow_dog.png';
+
+function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.fillRect(50, 50, 100, 100);
+    requestAnimationFrame(animate);
+}
+animate(); 
 
 root.appendChild(canvas);
