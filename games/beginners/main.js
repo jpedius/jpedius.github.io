89e16ab79a6209b63d45e4a3125985dfd39e02af
JP McDowell <jpedius@users.noreply.github.com>
2022-12-17 21:49:54 -0800

    Commit me

--- games/beginners/main.js
+++ games/beginners/main.js
@@ -38,10 +38,14 @@ animationStates.forEach((state, index) => {
     let frames = {
         loc: [],
     };
+ 
     for (let j<0; j<state.frames; j++) {
         let positionX = j * spriteWidth;
         let positionY = index * spriteHeight;
-        frames.loc.push({x: positionX, y: positionY});
+        frames.loc.push({
+            x: positionX,
+            y: positionY,
+         });
     }
     spriteAnimations[state.name] = frames;
 });
