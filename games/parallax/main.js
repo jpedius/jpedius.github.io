b95d00c99dee5c7ce0cfca220757791083baa3d2
JP McDowell <jpedius@users.noreply.github.com>
2022-12-19 00:31:57 -0800

    Commit me

--- games/parallax/main.js
+++ games/parallax/main.js
@@ -6,7 +6,7 @@ const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
 
-let gameSpeed = 10;
+let gameSpeed = 4;
 let gameFrame = 0;
 
 const backgroundLayer1 = new Image();
