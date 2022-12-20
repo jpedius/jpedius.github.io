41219828788e3ca5449e284ddd21985f17479065
JP McDowell <jpedius@users.noreply.github.com>
2022-12-20 14:50:44 -0800

    Commit me

--- games/parallax/main.js
+++ games/parallax/main.js
@@ -24,7 +24,7 @@ backgroundLayer4.src = 'backgroundLayers/layer-4.png';
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = 'backgroundLayers/layer-5.png';
 
-let sit = [0, 0, 0, 0, 0];
+//let sit = [0, 0, 0, 0, 0];
 
 window.addEventListener('load', function() {
 
@@ -39,11 +39,12 @@ window.addEventListener('load', function() {
     slide.addEventListener('change', function(e) {
         gameSpeed = e.target.value;
         showGameSpeed.innerHTML = e.target.value;
-        
+        /*
         if (sit[0] < 4) {
             console.log(e.target.value);
             sit[0] += 1;
-        } 
+        }
+        */
     }, false);
 
     class Layer {
@@ -64,21 +65,23 @@ window.addEventListener('load', function() {
         update() {
             this.speed = gameSpeed * this.speedModifier;
             this.x = (gameFrame * this.speed) % this.width;
-            
+            /*
             if (sit[1] < 4) {
                 console.log(this.speed, this.x);
                 sit[1] += 1;
             }
+            */
         }
         
         draw() {
             ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
             ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
-
+            /*
             if (sit[2] < 4) {
                 console.log('log: ' + this.x + ', ' + (this.x + this.width));
                 sit[2] += 1;
             }
+            */
         }
     }
 
@@ -97,20 +100,48 @@ window.addEventListener('load', function() {
         gameObject.forEach(object => {
             object.update();
             object.draw();
-
+            /*
             if (sit[3] < 4) {
                 console.log('update draw');
                 sit[3] += 1;
             }
+            */
         });
         gameFrame--;
-        
+        /*
         if (sit[4] < 4) {
             console.log(gameFrame);
             sit[4] += 1;
         }
-            
+        */   
         requestAnimationFrame(animate);
     }
     animate();
-}); 
+});
+
+const element = document.getElementById('garbage');
+let start, previousTimeStamp;
+let done = false
+
+function step(timestamp) {
+  if (start === undefined) {
+    start = timestamp;
+    }
+  const elapsed = timestamp - start;
+
+  if (previousTimeStamp !== timestamp) {
+    // Math.min() is used here to make sure the element stops at exactly 200px
+    const count = Math.min(0.1 * elapsed, 200);
+    element.style.transform = `translateX(${count}px)`;
+    if (count === 200) done = true;
+  }
+
+  if (elapsed < 2000) { // Stop the animation after 2 seconds
+    previousTimeStamp = timestamp;
+    if (!done) {
+      window.requestAnimationFrame(step);
+    }
+  }
+}
+
+window.requestAnimationFrame(step);
