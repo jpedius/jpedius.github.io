a57ff0a1da57f29d6226fd90ad749fff8ab6cc76
JP McDowell <jpedius@users.noreply.github.com>
2022-12-27 19:03:59 -0800

    Commit me

---
+++ tutorial/plots-two/main.js
@@ -0,0 +1,174 @@
+'use strict';
+
+(function($) {
+
+    let text = document.getElementById('text');
+    let words = document.getElementById('words');
+    
+    let howMany = [];
+    let previousOrNext = 0;
+    
+    let source = [];
+    
+    function setSentences() {
+
+        let src = '/tutorial/plots-two/files/' + words.value + '.txt';
+        $.get(src, function(data) {
+            
+            source = src.match( /[^\.!\?]+[\.!\?]+/g ).map((x) => (
+                x.trim().match( /[^\.!\?,]+[\.!\?,]+/g ).map((y) => (
+                    y.trim().split(' ');
+                ))
+            ));
+            
+            howMany = [];
+            previousOrNext = 0;
+        
+            text.value = howMany[previousOrNext];  
+        
+        }, 'text');
+    }
+
+    function setText() {
+        
+        text.value = howMany[previousOrNext];
+    }
+
+    function setPrevious() {
+        
+        if (previousOrNext <= 0) {
+            previousOrNext = howMany.length;
+        }
+        previousOrNext--;
+        
+        text.value = howMany[previousOrNext];
+    }
+    
+    function setPlay() {
+        
+        if (text.value !== '') {
+            speak(howMany[previousOrNext]);
+            text.blur();
+        }
+    }
+    
+    function setNext() {
+        
+        if (previousOrNext >= howMany.length - 1) {
+            previousOrNext = -1;
+        }
+        previousOrNext++;
+        
+        text.value = howMany[previousOrNext];
+    }
+    
+    function setWords() {
+        
+        if ('whole' === words.value) {
+            
+        }
+        else if ('comma' === words.value) {
+            
+        }
+        else if ('words' === words.value) {
+            howMany = source.flat(2);
+        }
+        previousOrNext = 0;
+        
+        text.value = howMany[previousOrNext];
+    }
+ 
+    // "Microsoft Zira - English (United States)"
+    // "Samantha"
+    // "Daniel"
+    // "Tessa"
+
+    let pitch = document.getElementById('pitch');
+    let rate = document.getElementById('rate');
+    let voice = document.getElementById('voice');
+
+    const synth = window.speechSynthesis;
+    let voices = [];
+
+    function populateVoiceList() {
+
+        // Sorted by
+        voices = synth.getVoices().sort(function (a, b) {
+            const aname = a.name.toUpperCase();
+            const bname = b.name.toUpperCase();
+            if (aname < bname) {
+                return -1;
+            } else if (aname == bname) {
+                return 0;
+            } else {
+                return +1;
+            }
+        });
+
+        voice.innerHTML = "";
+
+        for (let i = 0; i < voices.length; i++) {
+
+            if (voices[i].lang.slice(0, 3) === "en-") {
+
+                const option = document.createElement("option");
+                option.textContent = `${voices[i].name} (${voices[i].lang})`;
+                option.setAttribute("data-lang", voices[i].lang);
+                option.setAttribute("data-name", voices[i].name);
+
+                if (voices[i].name === "Microsoft Zira - English (United States)") {
+                    option.defaultSelected = true;
+                }
+                else if (voices[i].name === "Samantha") {
+                    option.defaultSelected = true;
+                }
+                else if (voices[i].name === "Tessa") {
+                    option.defaultSelected = true;
+                }
+
+                voice.appendChild(option);
+            }
+        }
+    }
+
+    populateVoiceList();
+    if (speechSynthesis.onvoiceschanged !== undefined) {
+        speechSynthesis.onvoiceschanged = populateVoiceList;
+    }
+
+    function speak(talk) {
+
+        if (synth.speaking) {
+            console.error("speechSynthesis.speaking");
+            return;
+        }
+
+        if (voice !== '') {
+
+            const utterThis = new SpeechSynthesisUtterance(talk);
+
+            utterThis.onend = function (event) {
+                console.log("SpeechSynthesisUtterance.onend");
+            };
+
+            utterThis.onerror = function (event) {
+                console.error("SpeechSynthesisUtterance.onerror");
+            };
+
+            const selectedOption =
+                voice.selectedOptions[0].getAttribute("data-name");
+
+            for (let i = 0; i < voices.length; i++) {
+                if (voices[i].name === selectedOption) {
+                    utterThis.voice = voices[i];
+                    break;
+                }
+            }
+
+            utterThis.pitch = pitch.value;
+            utterThis.rate = rate.value;
+            synth.speak(utterThis);
+        }
+    }
+ 
+})(jQuery);
