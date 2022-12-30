aa8da366c200b92b58175f598210909837a86bee
JP McDowell <jpedius@users.noreply.github.com>
2022-12-30 13:27:00 -0800

    Commit me

--- tutorial/plots-two/main.js
+++ tutorial/plots-two/main.js
@@ -22,7 +22,7 @@
     
     let src = {
 
-        sentences: '/tutorial/plots-two/files/' + sentences.value + '.txt';,
+        sentences: '/tutorial/plots-two/files/' + sentences.value + '.txt',
     };
     
     let howMany = [];
