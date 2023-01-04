691fe230ec9a41d089692db0a19a96eec22a1f17
JP McDowell <jpedius@users.noreply.github.com>
2023-01-03 20:01:22 -0800

    Commit me

--- tutorial/plots-two/main.js
+++ tutorial/plots-two/main.js
@@ -3,7 +3,7 @@
 (function($) {
 
     let sentences = document.getElementById('sentences');
-    sentences.addEventListener('change', setSentences, false);
+    sentences.addEventListener('change', setText, false);
     
     let text = document.getElementById('text');
     text.addEventListener('change', setText, false);
@@ -18,7 +18,7 @@
     next.addEventListener('click', setNext, false);
     
     let words = document.getElementById('words');
-    words.addEventListener('change', setWords, false);
+    words.addEventListener('change', setText, false);
     
     let src = {
 
@@ -33,55 +33,23 @@
     
     let howMany = [];
     let previousOrNext = 0;
-        
-    function setSentences() {
-    
-        src.sentences = (sentences.dataset.tutorial
-            + sentences.value
-            + sentences.dataset.txt);
-        
-         //text.value = howMany[previousOrNext];
-        setText();
-        //setText2();
-    }
-
-    function setText3() {
-    
-        $.get(src.sentences, function(data) {
-
-            let reWhole = /[^\.!\?]+[\.!\?]+/g;
-            let reComma = /[^\.!\?,]+[\.!\?,]+/g;
-
-            src.text = data.trim().match(reWhole).map(function (x) {
-                return x.trim().match(reComma).map(function (y) {
-                    return y.trim().split(' ');
-                });
-            });
-            
-            setWords();
-
-        }, 'text');
-    }
-    //setText();
     
     function setText() {
 
-        src.sentences = (sentences.dataset.tutorial2
+        src.sentences = (sentences.dataset.tutorial
             + sentences.value
             + sentences.dataset.txt);
             
         let my = new Request(src.sentences);
         fetch(my)
             .then((response) => {
-                //console.log('response', response);
                 if (!response.ok) {
                     throw new Error(`HTTP error, status = ${response.status}`);
                 }
                 return response.text();
             })
             .then((data) => {
-                console.log('data', data);
-                
+
                 let reWhole = /[^\.!\?]+[\.!\?]+/g;
                 let reComma = /[^\.!\?,]+[\.!\?,]+/g;
 
@@ -91,41 +59,32 @@
                     });
                 });
                 
-                setWords();
+                src.words = words.value;
+                if ('whole' === words.value) {
+                    howMany = src.text.map(function (x) {
+                        return x.map(function (y) {
+                            return y.join(' ');
+                        }).join(' ');
+                    });
+                }
+                else if ('comma' === words.value) {
+                    howMany = src.text.flat(1).map(function (x) {
+                        return x.join(' ');
+                    });
+                }
+                else if ('words' === words.value) {
+                    howMany = src.text.flat(2);
+                }
+                previousOrNext = 0;
                 
-                console.log('src', src);
+                text.value = howMany[previousOrNext];
             })
             .catch((error) => {
                 console.error(`Error: ${error.message}`);
             });
     }
     setText();
-    /*
-    let link = document.getElementById('words2');
-    link.addEventListener('click', getData, false);
     
-    function getData() {
-        
-        const myRequest = new Request('files/klaus.txt');
-
-        console.log('myRequest', myRequest);
-
-        fetch(myRequest)
-            .then((response) => {
-                console.log('response', response);
-                if (!response.ok) {
-                    throw new Error(`HTTP error, status = ${response.status}`);
-                }
-                return response.text();
-            })
-            .then((data) => {
-                console.log('data', data);
-            })
-            .catch((error) => {
-                console.error(`Error: ${error.message}`);
-            });
-    }
-    */
     function setPrevious() {
     
         if (previousOrNext <= 0) {
@@ -153,30 +112,6 @@
  
         text.value = howMany[previousOrNext];
     }
-    
-    function setWords() {
-        
-        src.words = words.value;
-        
-        if ('whole' === words.value) {
-            howMany = src.text.map(function (x) {
-                return x.map(function (y) {
-                    return y.join(' ');
-                }).join(' ');
-            });
-        }
-        else if ('comma' === words.value) {
-            howMany = src.text.flat(1).map(function (x) {
-                return x.join(' ');
-            });
-        }
-        else if ('words' === words.value) {
-            howMany = src.text.flat(2);
-        }
-        previousOrNext = 0;
-        
-        text.value = howMany[previousOrNext];
-    } 
  
     // "Microsoft Zira - English (United States)"
     // "Samantha"
