d589431fc1f6646fcabe9df5cffc0b3015f7e366
JP McDowell <jpedius@users.noreply.github.com>
2023-01-09 17:04:05 -0800

    Commit me

--- tutorial/medicine/main.js
+++ tutorial/medicine/main.js
@@ -8,27 +8,51 @@
     let medicine = [{
         "name": "labetalol",
         "image": "file/labetalol.jpg",
-        "description": "Labetalol HCL 300 mg tablet"
+        "description": "Labetalol HCL 300 mg tablet",
+        "pronunciation": {
+            "ipa": "/ləˈbɛtəlɔːl/",
+            "google": "luh·/bay/·tuh·laal"
+        }
     }, {
         "name": "lisinopril", 
         "image": "file/lisinopril.jpg", 
-        "description": "Lisinopril 20 mg tablet"
+        "description": "Lisinopril 20 mg tablet",
+        "pronunciation": {
+            "ipa": "/laɪˈsɪnəprɪl/",
+            "google": "luh·/si/·nuh·pruhl"
+        }
     }, {
         "name": "amlodipine besylate",
         "image": "file/amlodipine-besylate.jpg",
-        "description": "Amlodipine Besylate 10 mg tablet"
+        "description": "Amlodipine Besylate 10 mg tablet",
+        "pronunciation": {
+            "ipa": "/æmˈloʊdɪˌpiːn/", // besylate
+            "google": "am·/low/·duh·peen" // besylate
+        }
     }, { 
         "name": "spironolactone",
         "image": "file/spironolactone.jpg",
-        "description": "Spironolactone 25 mg tablet"
+        "description": "Spironolactone 25 mg tablet",
+        "pronunciation": {
+            "ipa": "/ˌspaɪroʊnoʊˈlæktoʊn/",
+            "google": "spai·ruh·now·/lak/·town"
+        }
     }, { 
         "name": "tamsulosin",
         "image": "file/tamsulosin.jpg",
-        "description": "Tamsulosin HCL 0.4 mg capsule"
+        "description": "Tamsulosin HCL 0.4 mg capsule",
+        "pronunciation": {
+            "ipa": "/tæmˈsuːləsɪn/",
+            "google": "tam·suh·/low/·sn"
+        }
     }, { 
         "name": "rosuvastatin calcium",
         "image": "file/rosuvastatin-calcium.jpg",
-        "description": "Rosuvastatin Calcium 5 mg tablet"
+        "description": "Rosuvastatin Calcium 5 mg tablet",
+        "pronunciation": { 
+            "ipa": "/roʊˈsuːvəstætɪn/", // calcium kˈælsi͡əm 
+            "google": "ruh·/soo/·vuh·sta·tuhn /kal/·see·uhm"
+        }
     }];
  
     let previousOrNext = 0;
