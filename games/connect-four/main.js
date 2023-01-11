c559d588f9fbef6cde1c87c8f796995d8ba9ce8d
JP McDowell <jpedius@users.noreply.github.com>
2023-01-10 18:52:51 -0800

    Commit me

--- games/connect-four/main.js
+++ games/connect-four/main.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const game = {
+
+    turn: true,
+}
+ 
 let a = [
     0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0,
@@ -14,7 +19,17 @@ console.log('a', a);
 document.addEventListener('click', event => {
 
     const target = event.target;
+    const isCell = target.classList.contains('grid-item');
+    const isDisabled = target.classList.contains('disabled');
+
+    if (isCell && !isDisabled) {
     
-    console.log('target', target);
-    console.log('class list', target.classList);
+        console.log('target', target);
+        console.log('class list', target.classList);
+        
+        target.classList.add('disabled');
+        target.classList.add(game.turn ? 'whale' : 'octopus');
+
+        game.turn = !game.turn;
+    } 
 });
