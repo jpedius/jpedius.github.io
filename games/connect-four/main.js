c8b6ca57e1165959396acde6d131943c5b03043f
JP McDowell <jpedius@users.noreply.github.com>
2023-01-13 16:19:24 -0800

    Commit me

--- games/connect-four/main.js
+++ games/connect-four/main.js
@@ -66,7 +66,7 @@ function winning_game(cell, len) {
     //game.a[]
     console.log(cell, len, game.turn);
     for (let i=0; i<game.a.length; i++) {
-        console.log(game.a[i]);
+        console.log(game.a[i], items[i]);
     }
     
     return null;
