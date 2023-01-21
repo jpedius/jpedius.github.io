a93347e184601f07a61d562701b226141cf8a8a6
JP McDowell <jpedius@users.noreply.github.com>
2023-01-21 13:41:14 -0800

    Commit me

--- games/tic-tac-toe/main.js
+++ games/tic-tac-toe/main.js
@@ -1,165 +1,182 @@
 'use strict';
 
-(function($) {
+const root = document.querySelector('#root');
 
-  const game = {
-
-    xTurn: true,
-    xState: [],
-    oState: [],
-    
-    winningStates: [
-      // Rows
-      ['0', '1', '2'],
-      ['3', '4', '5'],
-      ['6', '7', '8'],
-
-      // Columns
-      ['0', '3', '6'],
-      ['1', '4', '7'],
-      ['2', '5', '8'],
-
-      // Diagonal
-      ['0', '4', '8'],
-      ['2', '4', '6']
-    ]
-  }
-
-  document.addEventListener('click', event => {
-
-    const target = event.target
-    const isCell = target.classList.contains('grid-cell')
-    const isDisabled = target.classList.contains('disabled')
-
-    if (isCell && !isDisabled) {
-  
-      const cellValue = target.dataset.value
-
-      game.xTurn === true
-        ? game.xState.push(cellValue)
-        : game.oState.push(cellValue)
-
-      target.classList.add('disabled')
-      target.classList.add(game.xTurn ? 'x' : 'o')
-
-      game.xTurn = !game.xTurn
-
-      // If all cells are disabled, then its draw
-      if (!document.querySelectorAll('.grid-cell:not(.disabled)').length) {
-        document.querySelector('.game-over').classList.add('visible')
-        document.querySelector('.game-over-text').textContent = 'Draw!'
-      }
-
-      game.winningStates.forEach(winningState => {
-  
-        const xWins = winningState.every(state => game.xState.includes(state))
-        const oWins = winningState.every(state => game.oState.includes(state))
-
-        if (xWins || oWins) {
-          document.querySelectorAll('.grid-cell').forEach(cell => cell.classList.add('disabled'))
-          document.querySelector('.game-over').classList.add('visible')
-          document.querySelector('.game-over-text').textContent = xWins
-            ? 'X wins!'
-            : 'O wins!'
-        }
-      })
-    }
-  })
-
-  document.querySelector('.restart').addEventListener('click', () => {
-    
-    document.querySelector('.game-over').classList.remove('visible')
-    
-    document.querySelectorAll('.grid-cell').forEach(cell => {
-      cell.classList.remove('disabled', 'x', 'o')
-    })
-
-    game.xTurn = true
-    game.xState = []
-    game.oState = []
-  })
-/*
-  const game = {
+const game = {
 
     turn: true,
     
-    sTicTacToe: [
-      true, true, true,
-      true, true, true,
-      true, true, true,
-    ],
-    sDisabled: [
-      true, true, true,
-      true, true, true,
-      true, true, true,
-    ],
-
-    x: [],
-    o: [],
+    winner: 0,
     
-    winner: [
- 
-      // rows
-      ['0', '1', '2'],
-      ['3', '4', '5'],
-      ['6', '7', '8'],
-      
-      // columns
-      ['0', '3', '6'],
-      ['1', '4', '7'],
-      ['2', '5', '8'],
-      
-      // diagonal
-      ['0', '4', '8'],
-      ['2', '4', '6'],          
-    ],  
-  };
-
-  let root = document.querySelector('#root');
-  let sCenter = document.createElement('center');
-  
-  for (let i=0; i<3; i++) {
-
-    let sDiv = document.createElement('div');
-    sDiv.classList.add('sDiv');
+    allow: [
+        'Playing...',
+        'X Won',
+        'O Won',
+        'A Draw',
+    ],
     
-    for (let j=0; j<3; j++) {
-      
-      let sSpan = document.createElement('span'); 
-      sSpan.classList.add('sSpan');
-      
-      sSpan.innerHTML = '_';      
-      
-      sDiv.appendChild(sSpan);
+    levels: [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        
+        [0, 4, 8],
+        [2, 4, 8],    
+    ],
+};
+
+const text = root.children[1].children[0];
+text.innerText = game.allow[game.winner];
+
+document.addEventListener('click', event => {
+
+    const target = event.target;
+
+    const container = target.classList.contains('grid-container');
+    const items = target.classList.contains('grid-item');
+
+    if (items && !container && game.winner === 0) {
+    
+    }
+
+    text.innerText = game.allow[game.winner];
+});
+
+document.querySelector('.restart').addEventListener('click', () => {
+
+    document.querySelectorAll('.grid-item').forEach(item => {
+        item.classList.remove('disabled', 'x', 'o');
+    });
+
+    game.turn = true;
+    game.winner = 0;
+    //for (let i=0; i<game.levels.length; i++) {
+    //    game.levels[i] = 0;
+    //}
+});
+
+/*    
+    
+    
+    all: 0, 
+    
+    number: Number(root.children[0].dataset.length),
+    
+    connect: root.children[0].children,
+    
+    level: [0, 0, 0, 0, 0, 0, 0],
+    
+    position: [
+        
+        [false, false, false, true],
+        [false, false, false, true],
+        [false, false, false, true],
+
+        [false, false, false, true],
+        [false, false, false, false],
+        [false, false, false, false],
+        
+        [false, false, false, true],
+        [false, false, false, true],
+    ],
+};
+
+const text = root.children[1].children[0];
+text.innerText = game.draw[game.all];
+
+function game_items() {
+
+    let all = 0;
+    for (let i=0; i<game.connect.length; i++) {
+        
+        if (game.connect[i].classList.contains('disabled')) {
+        
+            all += 1;
+
+            let turn = game.turn ? 'whale' : 'octopus';
+            if (game.connect[i].classList.contains(turn)) {
+
+                let pos = game.position[i];
+                let idx = [
+                    [0, -8, -16, -24],
+                    [0, -7, -14, -21],
+                    [0, -6, -12, -18],
+                    [0,  1,   2,   3],
+                ];
+                  
+                for (let j=0; j<pos.length; j++) {
+                    if (pos[j] === true) {
+                        let step = 0;
+                        for (let k=0; k<idx[j].length; k++) {
+                            let x = i + idx[j][k];
+                            let y = game.connect[x].classList.contains(turn);
+                            if (y === true) { step += 1; }
+                        }
+                        if (step === 4) {
+                            return [turn, (game.turn ? 1 : 2)];
+                        }
+                    }
+                }
+            }
+        }
     }
     
-    sCenter.appendChild(sDiv);  
-  }
-  
-      
-      document.addEventListener('click', function(event) {
-      
-        //console.log(game, game.sDisabled, event, pos, game.sDisabled[pos]);
-        //console.log(game.sDisabled[event.target.classList[1]]);
-        //console.log(event.target.classList.contains['i0']);
-        console.log(event.target.classList);
-        //event.target.classList.contains('');
-        
-        
-        
-        
-        if (game.sDisabled[pos] === true) {
-          sSpan.innerHTML = game.turn ? 'X' : 'O';
-          game.turn = !game.turn;
-          game.sDisabled[pos] = false;
-        }
-        else {
-          sSpan.innerHTML = 'G';
-        }
-         
-      }, false);  
+    if (all === 42) { return [null, 3]; }
+    return [null, 0];
+}
 
-  root.appendChild(sCenter);
-  */
- 
-})(jQuery);
+document.addEventListener('click', event => {
+
+    const target = event.target;
+
+    const container = target.classList.contains('grid-container');
+    const items = target.classList.contains('grid-item');
+
+    if (items && !container && game.winner === null) {
+
+        let targetRow = Number(target.dataset.row);
+        if (game.level[targetRow] < game.number) {
+
+            let cell = null;
+            for (let i=0; i<game.connect.length; i++) {
+                let row = Number(game.connect[i].dataset.row);
+                let column = Number(game.connect[i].dataset.column);
+                if (row === targetRow && column === game.level[targetRow]) {
+                    cell = game.connect[i];
+                }
+            }
+
+            if (cell !== null) {
+                cell.classList.add('disabled');
+                cell.classList.add(game.turn ? 'whale' : 'octopus');
+                game.level[targetRow] += 1;
+                [game.winner, game.all] = game_items();
+                if (game.winner === null) {
+                    game.turn = !game.turn;
+                }
+            }
+        }
+    }
+
+    text.innerText = game.draw[game.all];
+});
+
+document.querySelector('.restart').addEventListener('click', () => {
+
+    document.querySelectorAll('.grid-item').forEach(item => {
+        item.classList.remove('disabled', 'whale', 'octopus');
+    });
+
+    game.turn = true;
+    game.winner = null;
+    game.all = 0;
+    for (let i=0; i<game.level.length; i++) {
+        game.level[i] = 0;
+    }
+});
+*/
+
