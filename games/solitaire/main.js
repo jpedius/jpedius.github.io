8a5a55eb991a9c067bceba7f535353148bda95c7
JP McDowell <jpedius@users.noreply.github.com>
2023-02-01 22:52:48 -0800

    Commit me

--- games/solitaire/main.js
+++ games/solitaire/main.js
@@ -17,36 +17,16 @@ function deck(cards, suits) {
         }
     }
     
-    console.log(items);
     return items;
 }
 
 function shuffle(array) {
 
-    let items = JSON.parse(JSON.stringify(array));
-    let currentIndex = items.length, randomIndex;
-
-    // While there remain elements to shuffle...
-    while (currentIndex !== 0) {
-
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-
-        // And swap it with the current element.
-        [items[currentIndex], items[randomIndex]] = [
-            items[randomIndex], items[currentIndex]];
-    }
-
-    return items;
-}
-
-function shuffle2(array) {
-
     let items = [...array];    
     let current = items.length;
     let rand = 0;
 
+    let q = '';
     while (current !== 0) {
         
         rand = Math.floor(Math.random() * current);
@@ -54,10 +34,10 @@ function shuffle2(array) {
         
         [items[current], items[rand]] = [items[rand], items[current]];
         
-        //console.log(items[current], items[rand], current, rand);
+        q += current + ' ' + rand ' ';
     }
 
-    console.log(items);
+    console.log(q);
     return items;
 }
 
