7479c2dfa93078a08ab5d7cb5443ad4e89520d3f
JP McDowell <jp.mcdowell@gmail.com>
2023-03-26 18:54:27 -0700

    Commit me

--- tutorial-two/words/main.js
+++ tutorial-two/words/main.js
@@ -1,6 +1,34 @@
 'use strict';
 
-console.log(pitch);
-console.log(rate);
-console.log(voice);
-console.log(voices);
+let previous = document.getElementById('previous');
+previous.addEventListener('click', clickPrevious, false);
+
+let play = document.getElementById('play');
+play.addEventListener('click', clickPlay, false);
+
+let next = document.getElementById('next');
+next.addEventListener('click', clickNext, false);
+
+function clickPrevious() {
+
+}
+
+function clickPlay() {
+
+}
+
+function clickNext() {
+
+}
+
+function consoleLog() {
+
+	console.log(previous);
+	console.log(play);
+	console.log(next);
+	console.log(pitch);
+	console.log(rate);
+	console.log(voice);
+	console.log(voices);
+}
+consoleLog();
