169f5ec68073b7181b601f426b85a67b1091b359
JP McDowell <jp.mcdowell@gmail.com>
2023-07-03 15:21:07 -0700

    Commit me

---
+++ games/alphabet/main.js
@@ -0,0 +1,87 @@
+'use strict';
+
+let alphabet = [lower, upper];
+
+let howMany = alphabet[0];
+let previousOrNext = 0;
+
+let text = document.getElementById('text');
+let textShowHide = true;
+
+let random = document.getElementById('random');
+let randomCheck = false;
+
+setValues();
+
+function shuffle(array) {
+
+	let items = JSON.parse(JSON.stringify(array));
+	let currentIndex = items.length, randomIndex;
+
+	// While there remain elements to shuffle...
+	while (currentIndex !== 0) {
+
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+
+		// And swap it with the current element.
+		[items[currentIndex], items[randomIndex]] = [
+		items[randomIndex], items[currentIndex]];
+	}
+
+	return items;
+}
+
+function clickSelectSentences() {
+	//previousOrNext = 0;
+	howMany = alphabet[sentences.options.selectedIndex];
+	if (randomCheck == true) {
+		howMany = shuffle(howMany);
+	}
+	return setValues();
+} 
+
+function clickButtonPrevious() {
+	if (previousOrNext <= 0) {
+		previousOrNext = howMany.length;
+	}
+	previousOrNext--;
+	return setValues();
+}
+
+function clickButtonPlay() {
+	speak(howMany[previousOrNext]);
+}
+
+function clickButtonNext() {
+	if (previousOrNext >= howMany.length - 1) {
+		previousOrNext = -1;
+	}
+	previousOrNext++;
+	return setValues();
+}
+
+function clickButtonText() {
+	textShowHide = !textShowHide;
+	text.value = textShowHide
+		? howMany[previousOrNext]
+		: '--';
+}
+
+function clickCheckRandom() {
+	randomCheck = !randomCheck;
+	clickSelectSentences();
+}
+
+function setValues() {
+    text.value = textShowHide
+        ? howMany[previousOrNext]
+        : '--';
+    return howMany[previousOrNext];
+}
+
+function clickButtonMode() {
+	let element = document.body;
+	element.classList.toggle('darkModeButton');
+}
