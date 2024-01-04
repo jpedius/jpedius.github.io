a4208d69572a46c7644ae1747e8c0f2ddff3ade6
JP McDowell <jp.mcdowell@gmail.com>
2024-01-03 23:00:01 -0800

    Commit me

--- code/go-webassembly/main.go
+++ code/go-webassembly/main.go
@@ -1,41 +1,135 @@
+// Copyright 2018 The Ebiten Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"image"
 	_ "image/png"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/examples/resources/images"
 )
 
-var img *ebiten.Image
+const (
+	screenWidth  = 240
+	screenHeight = 240
+)
+
+const (
+	tileSize = 16
+)
+
+var (
+	tilesImage *ebiten.Image
+)
 
 func init() {
-	var err error
-	img, _, err = ebitenutil.NewImageFromFile("img/gopher.png")
+	// Decode an image from the image file's byte slice.
+	img, _, err := image.Decode(bytes.NewReader(images.Tiles_png))
 	if err != nil {
 		log.Fatal(err)
 	}
+	tilesImage = ebiten.NewImageFromImage(img)
 }
 
-type Game struct{}
+type Game struct {
+	layers [][]int
+}
 
 func (g *Game) Update() error {
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	screen.DrawImage(img, nil)
+	w := tilesImage.Bounds().Dx()
+	tileXCount := w / tileSize
+
+	// Draw each tile with each DrawImage call.
+	// As the source images of all DrawImage calls are always same,
+	// this rendering is done very efficiently.
+	// For more detail, see https://pkg.go.dev/github.com/hajimehoshi/ebiten/v2#Image.DrawImage
+	const xCount = screenWidth / tileSize
+	for _, l := range g.layers {
+		for i, t := range l {
+			op := &ebiten.DrawImageOptions{}
+			op.GeoM.Translate(float64((i%xCount)*tileSize), float64((i/xCount)*tileSize))
+
+			sx := (t % tileXCount) * tileSize
+			sy := (t / tileXCount) * tileSize
+			screen.DrawImage(tilesImage.SubImage(image.Rect(sx, sy, sx+tileSize, sy+tileSize)).(*ebiten.Image), op)
+		}
+	}
+
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("TPS: %0.2f", ebiten.ActualTPS()))
 }
 
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 800, 600
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
 }
 
 func main() {
-	ebiten.SetWindowSize(800, 600)
-	ebiten.SetWindowTitle("Render an image")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	g := &Game{
+		layers: [][]int{
+			{
+				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
+				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 218, 243, 243, 243,
+				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
+				243, 218, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 244, 243,
+				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
+
+				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
+				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
+				243, 243, 244, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
+				243, 243, 243, 243, 243, 243, 243, 243, 243, 219, 243, 243, 243, 219, 243,
+				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
+
+				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
+				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
+				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
+				243, 218, 243, 243, 243, 243, 243, 243, 243, 243, 243, 244, 243, 243, 243,
+				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
+			},
+			{
+				0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+				0,   (25*11)+11,   0,   (25*12)+1,   0,   26,  27,  28,  29,  30,  31,  0,   0,   0,   0,
+				0,   0,   0,   0,   0,   51,  52,  53,  54,  55,  56,  0,   0,   0,   0,
+				0,   (25*11)+12,   0,   (25*12)+2,   0,   76,  77,  78,  79,  80,  81,  0,   0,   0,   0,
+				0,   0,   0,   0,   0,   101, 102, 103, 104, 105, 106, 0,   0,   0,   0,
+
+				0,   (25*11)+13,   0,   (25*12)+3,   0,   126, 127, 128, 129, 130, 131, 0,   0,   0,   0,
+				0,   0,   0,   0,   0,   303, 303, 245, 242, 303, 303, 0,   0,   0,   0,
+				0,   0,   0,   (25*12)+4,   0,   0,   0,   245, 242, 0,   0,   0,   0,   0,   0,
+				0,   0,   0,   0,   0,   0,   0,   245, 242, 0,   0,   0,   0,   0,   0,
+				0,   0,   0,   0,   0,   0,   0,   245, 242, 0,   0,   0,   0,   0,   0,
+
+				0,   58,  59,  60,  61,  0,   0,   245, 242, 0,   0,   0,   0,   0,   0,
+				0,   83,  84,  85,  86,  0,   0,   245, 242, 0,   0,   0,   186, 0,   0,
+				0,   108, 109, 110, 111, 0,   0,   245, 242, 0,   0,   0,   211,   0,   0,
+				0,   133, 134, 135, 136, 0,   0,   245, 242, 0,   0,   0,   0,   0,   0,
+				0,   0,   0,   0,   0,   0,   0,   245, 242, 0,   0,   0,   0,   0,   0,
+			},
+		},
+	}
+
+	ebiten.SetWindowSize(screenWidth*2, screenHeight*2)
+	ebiten.SetWindowTitle("Tiles (Ebitengine Demo)")
+	if err := ebiten.RunGame(g); err != nil {
 		log.Fatal(err)
 	}
 }
---
+++ code/go-webassembly/main.js
@@ -0,0 +1,3 @@
+// go mod tidy
+// GOOS=js GOARCH=wasm go build -o main.wasm
+// python3 -m http.server 8080
