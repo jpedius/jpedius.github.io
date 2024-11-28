49b86b2e3bcc47891c866ad24a3485a07345faef
JP McDowell <jp.mcdowell@gmail.com>
2024-11-28 00:12:22 -0800

    Commit me

--- therapy/reading/reading-five-times/main.js
+++ therapy/reading/reading-five-times/main.js
@@ -1,14 +1,40 @@
 'use strict';
 
-let myTitle = document.getElementById("myTitle");
+let title = document.getElementById("myTitle");
 
-function fn_time(t, num) {
+function fn_time(time, num) {
 	
 	let now = Date.now();
-	let doc = document.getElementById(t + "Input" + num).value = now;
-	
-	if (t === "stop") {
-		let title = myTitle.value.replaceAll(" ", "_").toLowerCase() + "_" + now;
-		document.getElementById("timeInput" + num).value = title;
+	document.getElementById(time + "Input" + num).value = now;
+
+	if (time === "stop") {
+
+		let startInput = document.getElementById("startInput" + num);
+		let stopInput  = document.getElementById("stopInput" + num);
+		let timeInput  = document.getElementById("timeInput" + num);
+
+		let elapsed = stopInput.value - startInput.value;
+		let seconds = Math.floor(elapsed / 1000);
+
+		let min = Math.floor(seconds / 60);
+		let sec = seconds - min * 60;
+
+		let a = title.value;
+		a = a.replaceAll(" ", "_");
+		a = a.toLowerCase();
+		a = a + "_" + String(num).toString();
+		a = a + "_" + String(min).toString();
+		a = a + "_" + String(sec).toString();
+		a = a + "_" + now;
+		timeInput.value = a;
+
+		console.log("----------------");
+		console.log(now);
+		console.log(time);
+		console.log(num);
+		console.log(elapsed);
+		console.log(seconds);
+		console.log(min);
+		console.log(sec);
 	}
 }
