2a458797e623288aadb4ba0d44532e6110840420
JP McDowell <jp.mcdowell@gmail.com>
2024-12-05 21:41:45 -0800

    Commit me

--- local-storage/main.js
+++ local-storage/main.js
@@ -1,7 +1,18 @@
 'use strict';
 
 let idReadingFiveTimes = document.getElementById("idReadingFiveTimes");
-let json = localStorage.getItem("reading-five-times");
-let settings = JSON.parse(json);
-let text = JSON.stringify(settings, null, "\t");
+let idSpellingAWord = document.getElementById("idSpellingAWord");
+
+let json = null;
+let settings = null;
+let text = null;
+
+json = localStorage.getItem("reading-five-times");
+settings = JSON.parse(json);
+text = JSON.stringify(settings, null, "\t");
 idReadingFiveTimes.value = text;
+
+json = localStorage.getItem("spelling-a-word");
+settings = JSON.parse(json);
+text = JSON.stringify(settings, null, "\t");
+idSpellingAWord.value = text;
