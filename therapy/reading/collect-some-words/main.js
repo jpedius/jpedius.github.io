6792c302e5bc078ab814a36398c22d2c5e4547ea
JP McDowell <jp.mcdowell@gmail.com>
2024-12-27 14:34:59 -0800

    Commit me

--- therapy/reading/collect-some-words/main.js
+++ therapy/reading/collect-some-words/main.js
@@ -3,7 +3,6 @@
 // console.log(words);
 
 let idDiv = document.getElementById("idDiv");
-let idShow = document.getElementById("idShow");
 let idSave = document.getElementById("idSave");
 
 let NUMBER_OF_SMALL_WORDS = 3
@@ -60,18 +59,23 @@ else {
 	todaysWords = small.concat(large);
 }
 
-let spans = [];
-let show = false;
+let showHidden = [];
+let textArea = [];
+
 for (let i=0; i<todaysWords.length; i++) {
 
     let div = document.createElement('div');
     div.classList.add('myDiv');
 
+    let write = document.createElement('textarea');
+    write.classList.add('myTextarea');
+    write.rows = 4;
+    write.cols = 40;
+
 	let span = document.createElement('span');
 	span.classList.add('mySpan');
 	span.innerHTML = ' ' + todaysWords[i] + ' ';
 	span.style.display = 'none';
-	spans.push(span);
 
     let play = document.createElement('button');
     play.classList.add('myButton');
@@ -80,25 +84,48 @@ for (let i=0; i<todaysWords.length; i++) {
         fn_speak(todaysWords[i]);
     });
 
-    div.appendChild(play);
-    div.appendChild(span);
+    let show = document.createElement('button');
+    show.classList.add('myButton');
+    show.innerHTML = 'Show';
+    show.addEventListener('click', () => {
+        showHidden[i] = !showHidden[i];
+        show.innerHTML = showHidden[i] ? 'Hide' : 'Show';
+        span.style.display = showHidden[i] ? 'inline' : 'none';
+    });
+
+    let div1 = document.createElement('div');
+    div1.appendChild(write);
+    div.appendChild(div1);
+
+    let div2 = document.createElement('div');
+    div2.appendChild(play);
+    div2.appendChild(show);
+    div2.appendChild(span);
+    div.appendChild(div2);
+
+    let hr = document.createElement('hr');
+    div.appendChild(hr);
 
 	idDiv.appendChild(div);
-}
 
-function fn_show_or_hidden() {
+	showHidden.push(false);
 
-    show = !show;
-    for (let i=0; i<spans.length; i++) {
-        spans[i].style.display = show ? 'inline' : 'none';
-    }
-    idShow.innerHTML = show ? 'Hide' : 'Show';
+	let texts = pastWords[lastItemLength].text;
+	write.innerHTML = texts[i];
+
+	textArea.push(write);
 }
 
 function fn_save() {
 
 	if (todaysNumberAlreadyDone !== true) {
-		pastWords.push({ date: day, words: todaysWords });
+
+		let text = [];
+		for (let i=0; i<textArea.length; i++) {
+			text.push(textArea[i].value);
+		}
+
+		pastWords.push({ date: day, words: todaysWords, text: text });
 		localStorage.setItem(local_storage, JSON.stringify(pastWords));
 		idSave.disabled = true;
 	}
