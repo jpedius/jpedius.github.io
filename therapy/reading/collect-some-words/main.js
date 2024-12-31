c7ca968244260bfed4289fbbf4dd78d56335ee3e
JP McDowell <jp.mcdowell@gmail.com>
2024-12-31 13:40:44 -0800

    Commit me

--- therapy/reading/collect-some-words/main.js
+++ therapy/reading/collect-some-words/main.js
@@ -1,6 +1,9 @@
 'use strict';
 
-let id_collect = document.getElementById("idCollect");
+// python3 -m http.server 3000
+
+let id_select = document.getElementById("idSelect");
+let id_collection = document.getElementById("idCollection");
 let id_save_words = document.getElementById("idSaveWords");
 let id_input = document.getElementById("idInput");
 
@@ -12,12 +15,34 @@ if (past_words === null) { past_words = []; }
 let NUMBER_OF_SMALL_WORDS = 3
 let NUMBER_OF_SMALL_SLICES = 5;
 let NUMBER_OF_LARGE_SLICES = 10;
+let NUMBER_OF_SELECTION = NUMBER_OF_SMALL_SLICES + NUMBER_OF_LARGE_SLICES;
 
-function fn_select_set(previous) {
+function fn_select_words(previous_words) {
+
+	id_select.innerHTML = '';
+
+	for (let i=0; i<previous_words.length; i++) {
+		const option = document.createElement('option');
+		option.textContent = `${previous_words[i].date}`;
+		id_select.appendChild(option);
+	}
+
+	if (id_select.options.length !== 0) {
+		const last = id_select.options.length - 1;
+		id_select.options[last].selected = true;
+	}
+
+	const add = document.createElement('option');
+	add.textContent = "Add Words ...";
+	id_select.appendChild(add);
+}
+fn_select_words(past_words);
+
+function fn_remaining_words(previous_words) {
 
 	let past = [];
-	for (let i=0; i<previous.length; i++) {
-		let w = previous[i].words;
+	for (let i=0; i<previous_words.length; i++) {
+		let w = previous_words[i].words;
 		past = past.concat(w);
 	}
 
@@ -63,117 +88,171 @@ function fn_day() {
 	return day;
 }
 
-function fn_todays_words(previous) {
+function fn_new_selection(previous_words) {
 
-	let day = fn_day();
-	let last = previous.length - 1;
+	let the_words = fn_remaining_words(previous_words);
 
-	let the_words = [];
 	let the_text = [];
-	if (last !== -1 && previous[last].date === day) {
-		the_words = previous[last].words;
-		the_text = previous[last].text;
-		id_save_words.disabled = true;
-	}
-	else {
-		the_words = fn_select_set(previous);
-		for (let i=0; i<the_words.length; i++) {
-			the_text.push('');
-		}
+	for (let i=0; i<the_words.length; i++) {
+		the_text.push('');
 	}
 
-	return [the_words, the_text];
+	return {
+		date: fn_day(),
+		words: the_words,
+		text: the_text,
+	};
+}
+let new_selection = fn_new_selection(past_words);
+
+function fn_item_selection(previous_words, new_words) {
+
+	let item = id_select.options.selectedIndex;
+	let add_words = id_select.options.length - 1;
+
+	if (add_words === item) {
+		return new_words;
+	}
+
+	return previous_words[item];
+}
+let item_selection = fn_item_selection(past_words, new_selection);
+
+function fn_create() {
+
+	let create = [];
+
+	for (let i=0; i<NUMBER_OF_SELECTION; i++) {
+
+		let div0 = document.createElement('div');
+		div0.classList.add('myDiv');
+
+		let write = document.createElement('textarea');
+		write.classList.add('myTextarea');
+		write.value = fn_create_element_text(i);
+		write.rows = 4;
+		write.cols = 40;
+
+		let span = document.createElement('span');
+		span.classList.add('mySpan');
+		span.innerHTML = '-'.repeat(8);
+
+		let word = document.createElement('button');
+		word.classList.add('myButton');
+		word.innerHTML = 'Play Word';
+		word.addEventListener('click', () => {
+			fn_add_event_listener_word(i);
+		});
+
+		let text = document.createElement('button');
+		text.classList.add('myButton');
+		text.innerHTML = 'Play Text';
+		text.addEventListener('click', () => {
+			fn_add_event_listener_text(i, write);
+		});
+
+		let show = document.createElement('button');
+		show.classList.add('myButton');
+		show.innerHTML = 'Show';
+		show.addEventListener('click', () => {
+			create[i].tf = !create[i].tf;
+			show.innerHTML = create[i].tf ? 'Hide' : 'Show';
+			fn_add_event_listener_show(i, create[i].tf, span);
+		});
+
+		create.push({
+			write: write,
+			span: span,
+			tf: false,
+			show: show,
+		});
+
+		let div1 = document.createElement('div');
+		div1.appendChild(write);
+		div0.appendChild(div1);
+
+		let div2 = document.createElement('div');
+		div2.appendChild(word);
+		div2.appendChild(text);
+		div2.appendChild(show);
+		div2.appendChild(span);
+		div0.appendChild(div2);
+
+		let hr = document.createElement('hr');
+		div0.appendChild(hr);
+
+		id_collection.appendChild(div0);
+	}
+
+	return create;
+}
+let item_collection = fn_create();
+
+function fn_add_event_listener_word(num) {
+	let event = (item_selection.words)[num];
+	fn_speak(event);
 }
 
-function fn_create_single_sentence(the_word, the_text, the_show) {
-
-	let div = document.createElement('div');
-	div.classList.add('myDiv');
-
-	let write = document.createElement('textarea');
-	write.classList.add('myTextarea');
-	write.value = the_text;
-	write.rows = 4;
-	write.cols = 40;
-
-	let playWord = document.createElement('button');
-	playWord.classList.add('myButton');
-	playWord.innerHTML = 'Play Word';
-	playWord.addEventListener('click', () => {
-		fn_speak(the_word);
-	});
-
-	let playText = document.createElement('button');
-	playText.classList.add('myButton');
-	playText.innerHTML = 'Play Text';
-	playText.addEventListener('click', () => {
-		if (write.value !== '') {
-			fn_speak(write.value);
-		}
-	});
-
-	let show = document.createElement('button');
-	show.classList.add('myButton');
-	show.innerHTML = 'Show';
-	show.addEventListener('click', () => {
-		the_show = !the_show;
-		show.innerHTML = the_show ? 'Hide' : 'Show';
-		span.style.display = the_show ? 'inline' : 'none';
-	});
-
-	let span = document.createElement('span');
-	span.classList.add('mySpan');
-	span.innerHTML = ' ' + the_word + ' ';
-	span.style.display = 'none';
-
-	let div1 = document.createElement('div');
-	div1.appendChild(write);
-	div.appendChild(div1);
-
-	let div2 = document.createElement('div');
-	div2.appendChild(playWord);
-	div2.appendChild(playText);
-	div2.appendChild(show);
-	div2.appendChild(span);
-	div.appendChild(div2);
-
-	let hr = document.createElement('hr');
-	div.appendChild(hr);
-
-	id_collect.appendChild(div);
-
-	return write;
+function fn_create_element_text(num) {
+	let event = (item_selection.text)[num];
+	return event;	
 }
 
-let [todays_words, today_text] = fn_todays_words(past_words);
-let show_hidden = [];
-let save_writes = []
+function fn_add_event_listener_text(num, write) {
+	let event = (item_selection.text)[num];
+	if (write.value !== '') {
+		fn_speak(write.value);
+	}
+}
 
-for (let i=0; i<todays_words.length; i++) { 
-	show_hidden.push(false);
-	let save = fn_create_single_sentence(todays_words[i], today_text[i], show_hidden[i]);
-	save_writes.push(save);
+function fn_add_event_listener_show(num, show, span) {
+	let event = (item_selection.words)[num];
+	let repeat = '-'.repeat(8); // event.length
+	span.innerHTML = show ? event : repeat;
+}
+
+function fn_change_words() {
+
+	item_selection = fn_item_selection(past_words, new_selection);
+
+	for (let i=0; i<NUMBER_OF_SELECTION; i++) {
+		let item = item_collection[i];
+		item.write.value = (item_selection.text)[i];
+		item.span.innerHTML = '-'.repeat(8);
+		item.tf = false;
+		item.show.innerHTML = 'Show';
+	}
 }
 
 function fn_save_words() {
 
-	if (id_save_words.disabled === false) {
+	let the_date = fn_day();
 
-		let text = [];
-		for (let i=0; i<save_writes.length; i++) {
-			text.push(save_writes[i].value);
-		}
-
-		past_words.push({
-			date: fn_day(),
-			words: todays_words,
-			text: text 
-		});
-		
-		localStorage.setItem(local_storage, JSON.stringify(past_words));
-
-		id_save_words.disabled = true;
+	let the_words = [];
+	for (let i=0; i<NUMBER_OF_SELECTION; i++) {
+		let item = (item_selection.words)[i];
+		the_words.push(item);
 	}
+
+	let the_texts = [];
+	for (let i=0; i<NUMBER_OF_SELECTION; i++) {
+		let item = item_collection[i];
+		let write = item.write.value;
+		the_texts.push(write);
+	}
+
+	past_words.push({
+		date: the_date,
+		words: the_words,
+		text: the_texts,
+	});
+
+	localStorage.setItem(local_storage, JSON.stringify(past_words));
+
+	id_save_words.disabled = true;
+}
+if (item_selection.date === fn_day()) {
+	id_save_words.disabled = true;
 }
 
 function fn_delete_words() {
