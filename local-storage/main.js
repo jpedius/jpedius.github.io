1135468857e74fe2fbfc807a7795785d747e05a2
JP McDowell <jp.mcdowell@gmail.com>
2024-12-31 18:27:27 -0800

    Commit me

--- local-storage/main.js
+++ local-storage/main.js
@@ -8,35 +8,33 @@ let idCollectSomeWords = document.getElementById("idCollectSomeWords");
 let idTextWords = document.getElementById("idTextWords");
 let idSaveWords = document.getElementById("idSaveWords");
 
-let json = null;
-let settings = null;
-let text = null;
+let json1 = localStorage.getItem("reading-five-times");
+let settings1 = JSON.parse(json1);
+let text1 = JSON.stringify(settings1, null, "\t");
+idReadingFiveTimes.value = text1;
 
-json = localStorage.getItem("reading-five-times");
-settings = JSON.parse(json);
-text = JSON.stringify(settings, null, "\t");
-idReadingFiveTimes.value = text;
+let json2 = localStorage.getItem("spelling-a-word");
+let settings2 = JSON.parse(json2);
+let text2 = JSON.stringify(settings2, null, "\t");
+idSpellingAWord.value = text2;
 
-json = localStorage.getItem("spelling-a-word");
-settings = JSON.parse(json);
-text = JSON.stringify(settings, null, "\t");
-idSpellingAWord.value = text;
+let json3 = localStorage.getItem("sentence-generator");
+let settings3 = JSON.parse(json3);
+let text3 = JSON.stringify(settings3, null, "\t");
+idSentenceGenerator.value = text3;
 
-json = localStorage.getItem("sentence-generator");
-settings = JSON.parse(json);
-text = JSON.stringify(settings, null, "\t");
-idSentenceGenerator.value = text;
-
-json = localStorage.getItem("collect-some-words");
-// if (json !== null) {
-// 	settings = JSON.parse(json);
-// 	text = JSON.stringify(settings, null, "\t");
-// 	idCollectSomeWords.value = text;
-// }
+let json4 = localStorage.getItem("collect-some-words");
+let settings4 = JSON.parse(json4);
+let text4 = JSON.stringify(settings4, null, "\t");
+idCollectSomeWords.value = text4;
 
 function fn_save_words() {
 
 	localStorage.removeItem("collect-some-words");
+
 	let text = idTextWords.value;
+	text = text.replaceAll('\t', '');
+	text = text.replaceAll('\n', '');
+
 	localStorage.setItem("collect-some-words", text);
 }
