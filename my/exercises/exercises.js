f7cf604365203a3645c6b661e3a99806d2363870
JP McDowell <jp.mcdowell@gmail.com>
2025-07-19 15:32:31 -0700

    Commit me

---
+++ my/exercises/exercises.js
@@ -0,0 +1,120 @@
+'use strict';
+
+export function fn_exercises() {
+
+	const items = [{
+		group: 'Set 07-16-2025',
+		options: [{
+			name: 'Front Squat',
+			key: 'front-squat',
+			image: 'front-squat.jpeg',
+			exercises: [
+				'Rest a barbell across the front of your shoulders.',
+				'Slowly lower yourself into a squat position by bending your knees, pushing your buttocks backwards and keeping your chest upright.',
+				'Go as low as feels comfortable without letting your knees come past your toes.',
+				'Then return to the starting position by driving through your heels, extending your knees and extending your hips.',
+				'At the top of the movement lock your knees and hips into extension by squeezing your quadriceps and glutes.',
+				'Repeat for the desired number of repetitions.',
+			],
+			instructions: [
+				[ 'Repeat',   '10 Times' ],
+				[ 'Complete', '2 Sets' ],
+			],
+		}, {
+			name: 'Kayak',
+			key: 'kayak',
+			image: 'kayak.jpeg',
+			exercises: [
+				'Sit with weight bar held in both hands (palms up, or palms down, whatever is easier).',
+				'Lean back until your abdominal muscles engage, then "kayak" with the bar, performing figure 8 patterns right and left. (bottom pictures).',
+			],
+			instructions: [
+				[ 'Repeat',   '10 Times' ],
+				[ 'Complete', '2 Sets' ],
+			],
+		}, {
+			name: 'Elevated Plank on Elbows',
+			key: 'elevated-plank-on-elbows',
+			image: 'elevated-plank-on-elbows.jpeg',
+			exercises: [
+				'Place your elbows on an elevated surface.',
+				'Keep your forearms neutral, thumbs up and round your upper back/ protract your scapulae to activate your shoulder stabilizers.',
+				'Remember to keep shoulders over elbows, and drop your hips to feel your abdominal muscles engage.',
+			],
+			instructions: [
+				[ 'Repeat',   '4 Times' ],
+				[ 'Complete', '20 Sets' ],
+			],
+		}, {
+			name: 'Goodmorning',
+			key: 'goodmorning',
+			image: 'goodmorning.jpeg',
+			exercises: [
+				'Hold a weight bar in your elbows.',
+				'Feet shoulder width apart and engage core muscles.',
+				'Hold a weight bar in your elbows, crossing hands on the chest.',
+				'Hinge at the hip, slowly bringing your chest and shoulders towards the floor until tension is felt in hamstrings and gluts, think about pushing your hips back toward the wall behind you.',
+				'Keep back straight and chest up.',
+				'Keep shins perpendicular to floor so knees do not move forward and buttocks may move back *keeping back straight and core engaged.',
+			],
+			instructions: [
+				[ 'Repeat',   '10 Times' ],
+				[ 'Complete', '2 Sets' ],
+			],
+		}],
+	}, {
+		group: 'Set 06-18-2025',
+		options: [{
+			name: 'Bear in the Woods',
+			subname: 'Monster Walks',
+			key: 'bear-in-the-woods',
+			image: 'bear-in-the-woods-2.jpeg',
+			exercises: [
+				'Place a band around legs just above the knees or just below the knees for a little more difficulty.',
+				'With feet hip width apart, press the hips back slightly and bend the knees to put the body into a micro squat.',
+				'In a controlled motion, take a step forward but have the leg moving forward sweep just above the floor in a C motion.',
+				'Alternating legs, keep the resistance tight through the movement as you travel forward.',
+				'Forwards <> backwards',
+				'Zig-zag pattern',
+			],
+			instructions: [
+				[ 'Repeat',   '20 Times' ],
+				[ 'Complete', '4 Sets' ],
+			],
+		}, {
+			name: 'Like a Crab - Stand Tall',
+			subname: 'Lateral Monster Walk Elastic Band at Thighs Side Steps',
+			key: 'like-a-crab',
+			image: 'like-a-crab.jpeg',
+			exercises: [
+				'Place a looped elastic band around both thighs.',
+				'Next, bend your knees and step to the side while keeping tension on the band the entire time.',
+				'After taking sidesteps to the side in one direction, reverse the direction taking sidesteps until you return to the starting position.',
+				'Repeat.',
+			],
+			instructions: [
+				[ 'Hold',     '1 Second' ],
+				[ 'Complete', '1 Set' ],
+				[ 'Perform',  '1 Times a Day' ],
+			],
+		}, {
+			name: 'Like a Crab - Squat Position',
+			subname: 'Lateral Monster Walk Elastic Band at Thighs Side Steps',
+			key: 'like-a-crab',
+			image: 'like-a-crab.jpeg',
+			exercises: [
+				'Place a looped elastic band around both thighs.',
+				'Next, bend your knees and step to the side while keeping tension on the band the entire time.',
+				'After taking sidesteps to the side in one direction, reverse the direction taking sidesteps until you return to the starting position.',
+				'Repeat.',
+			],
+			instructions: [
+				[ 'Hold',     '1 Second' ],
+				[ 'Complete', '1 Set' ],
+				[ 'Perform',  '1 Times a Day' ],
+			],
+		}],
+	}];
+
+	return items;
+}
---
+++ my/exercises/main.js
@@ -0,0 +1,185 @@
+'use strict';
+
+import { fn_speak } from '../assets/module/voice.js';
+import { fn_exercises } from './exercises.js';
+
+function fn_image(div, selected) {
+
+	let div1 = document.createElement('div');
+	div.appendChild(div1);
+
+	let img = document.createElement('img');
+	img.classList.add('myImage');
+	img.src = '/my/assets/images/exercises/' + selected.image;
+	div1.appendChild(img);
+}
+
+function fn_play_and_text(div, selected) {
+
+	let container = document.createElement('div');
+	container.classList.add('myExercisesContainer');
+	div.appendChild(container);
+
+	for (let i=0; i<selected.exercises.length; i++) {
+
+		let grid1 = document.createElement('div');
+		grid1.classList.add('myExercisesGrid');
+		container.appendChild(grid1);
+
+		let play = document.createElement('button');
+		play.classList.add('myExercisesPlay');
+		play.innerHTML = 'Play';
+		play.addEventListener('click', () => {
+			fn_speak(selected.exercises[i]);
+		});
+		grid1.appendChild(play);
+
+		let grid2 = document.createElement('div');
+		grid2.classList.add('myExercisesGrid');
+		grid2.innerHTML = selected.exercises[i];
+		container.appendChild(grid2);
+	}
+}
+
+function fn_instructions(div, selected) {
+
+	let container = document.createElement('div');
+	container.classList.add('myInstructionsContainer');
+	div.appendChild(container);
+
+	let grid1 = document.createElement('div');
+	grid1.classList.add('myInstructionsGrid');
+	container.appendChild(grid1);
+
+	let grid2 = document.createElement('div');
+	grid2.classList.add('myInstructionsGridSpan');
+	container.appendChild(grid2);
+
+	let hr = document.createElement('hr');
+	grid2.appendChild(hr);
+
+	for (let i=0; i<selected.instructions.length; i++) {
+
+		let grid3 = document.createElement('div');
+		grid3.classList.add('myInstructionsGrid');
+		container.appendChild(grid3);
+
+		let grid4 = document.createElement('div');
+		grid4.classList.add('myInstructionsGrid');
+		grid4.innerHTML = selected.instructions[i][0];
+		container.appendChild(grid4);
+
+		let grid5 = document.createElement('div');
+		grid5.classList.add('myInstructionsGrid');
+		grid5.innerHTML = selected.instructions[i][1];
+		container.appendChild(grid5);
+	}
+}
+
+function fn_remove(div) {
+
+	while (div.firstChild) {
+		div.removeChild(div.firstChild);
+	}
+}
+
+function fn_add(div, select, items) {
+
+	let index = select.options.selectedIndex;
+	let selected = items.exercises[index];
+
+	if (selected.image !== undefined) {
+		fn_image(div, selected);
+	}
+	if (selected.exercises !== undefined) {
+		fn_play_and_text(div, selected);
+	}
+	if (selected.instructions !== undefined) {
+		fn_instructions(div, selected);
+	}
+}
+
+function fn_select(main, items) {
+
+	let select = document.createElement('select');
+	select.classList.add('mySelect');
+	select.innerHTML = '';
+	select.addEventListener('change', () => {
+		fn_remove(main.children[1]);
+		fn_add(main.children[1], select, items);
+	});
+	main.appendChild(select);
+
+	for (let i=0; i<items.optgroup.length; i++) {
+        const optgroup = document.createElement('optgroup');
+        optgroup.label = items.optgroup[i].group;
+        select.appendChild(optgroup);
+        for (let j=0; j<items.optgroup[i].options.length; j++) {
+            const option = document.createElement('option');
+            option.textContent = items.optgroup[i].options[j].name;
+            optgroup.appendChild(option);
+        }
+    }
+
+	return select;
+}
+
+function fn_items_div(main, items, select) {
+
+	let div = document.createElement('div');
+	main.appendChild(div);
+
+	fn_remove(div);
+	fn_add(div, select, items);
+}
+
+function fn_header(element, title) {
+
+	document.title = title;
+
+	let header = document.createElement('header');
+	header.classList.add('myHeader');
+	element.appendChild(header);
+
+	let div = document.createElement('div');
+	div.innerHTML = title;
+	header.appendChild(div);
+
+	let hr = document.createElement('hr');
+	header.appendChild(hr);
+}
+
+function fn_main(element, items) {
+
+	let main = document.createElement('main');
+	element.appendChild(main);
+
+	let select = fn_select(main, items);
+
+	fn_items_div(main, items, select);
+}
+
+function fn_app() {
+
+	let optgroup = fn_exercises();
+
+	let group = [];
+	for (let i=0; i<optgroup.length; i++) {
+		group = group.concat(optgroup[i].group)
+	}
+
+	let exercises = [];
+	for (let i=0; i<optgroup.length; i++) {
+		exercises = exercises.concat(optgroup[i].options);
+	}
+
+	let element = document.getElementById('main');
+
+	fn_header(element, 'Exercises');
+	fn_main(element, {
+		optgroup: optgroup,
+		group: group,
+		exercises: exercises,
+	});
+}
+fn_app();
