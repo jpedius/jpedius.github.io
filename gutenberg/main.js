0e78eab0d1259b71096f591d01b8016489540db6
JP McDowell <jp.mcdowell@gmail.com>
2026-05-19 16:46:10 -0700

    Commit me

--- gutenberg/main.js
+++ gutenberg/main.js
@@ -4,10 +4,133 @@ import { fn_speak } from "../js/speak.js";
 import { MyStyle } from "../js/style.js";
 import { MyHTML, MyEntry } from "../js/html.js";
 
-function fn_main(body, entry, style) {
+import { fn_books, MyBook } from "./books.js";
+
+function fn_request(object, books) {
+
+	let url = books[object.select.selectedIndex].key;
+
+	fetch(new Request(url))
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error(`HTTP error, status = ${response.status}`);
+			}
+			return response.text();
+		})
+		.then((data) => {
+
+			let html = new MyHTML();
+
+			while (object.text.firstChild) {
+				object.text.removeChild(object.text.firstChild);
+			}
+
+			object.book = new MyBook(data);
+
+			let array = object.book.array();
+			for (let i=0; i<array.length; i++) {
+				let div = html.div(object.text, ["myText"]);
+				for (let j=0; j<array[i].length; j++) {
+					html.span(div, ["myDisabled", "mySpan"], array[i][j]);
+				}
+			}
+
+			object.book.span(object.text, "myDisabled", false);
+
+			object.number.innerHTML = 1;
+			object.total.innerHTML = object.book.total();
+		})
+		.catch((error) => {
+			console.error(`Error: ${error.message}`);
+		});
+}
+
+function fn_main(body, style) {
+
+	let html = new MyHTML();
+	let object = { "book": null };
+	let books = fn_books();
 
 	let main = document.createElement("main");
 	body.appendChild(main);
+
+	style.add(`
+		.myDiv {
+			font: 20px --main-font, sans-serif;
+		    padding: 5px;
+		}
+		.myButton {
+			margin: 0px 10px 0px 0px;
+		}
+		.myArea {
+			height: 800px;
+			overflow: auto;
+			width: 1200px;
+		}
+		.myText {
+			font: 30px --main-font, sans-serif;
+		    padding: 5px;
+		}
+		.mySpan {
+			margin: 0px 10px 0px 0px;
+		}
+		.myDisabled {
+			color: #ccc;
+		}
+	`);
+
+	let div = html.div(main, ["myDiv"]);
+
+	object.select = html.select(div, ["myDiv"], (event) => {
+		fn_request(object, books);
+	});
+	html.options(object.select, books, true);
+
+	div = html.div(main, ["myDiv"]);
+
+	html.button(div, ["myDiv", "myButton"], "Previous", (event) => {
+
+		object.book.span(object.text, "myDisabled", true);
+		object.number.innerHTML = object.book.previous();
+		object.book.span(object.text, "myDisabled", false);
+	});
+
+	html.button(div, ["myDiv", "myButton"], "Play", (event) => {
+		fn_speak(object.book.play());
+	});
+
+	html.button(div, ["myDiv", "myButton"], "Next", (event) => {
+
+		object.book.span(object.text, "myDisabled", true);
+		object.number.innerHTML = object.book.next();
+		object.book.span(object.text, "myDisabled", false);
+	});
+
+	object.number = html.number(div, ["myDiv"], 1);
+
+	html.number(div, ["myDiv"], "/");
+
+	object.total = html.number(div, ["myDiv", "myButton"], "");
+
+	html.button(div, ["myDiv", "myButton"], "Go To", (event) => {
+
+		if (Number(object.goto.value) === NaN) { return }
+		if (Number(object.goto.value) <= 0) { return }
+		if (Number(object.goto.value) > Number(object.total.innerHTML)) { return }
+
+		object.book.span(object.text, "myDisabled", true);
+		object.number.innerHTML = object.book.go_to(object.goto.value);
+		object.goto.value = "";
+		object.book.span(object.text, "myDisabled", false);
+	});
+
+	object.goto = html.input(div, ["myDiv", "myButton"], 4);
+
+	div = html.div(main, ["myDiv"]);
+
+	object.text = html.div(div, ["myArea"]);
+
+	fn_request(object, books);
 }
 
 function fn_app(title) {
@@ -29,7 +152,7 @@ function fn_app(title) {
 		padding: 5px;
 	}`);
 
-	fn_main(body, entry, style);
+	fn_main(body, style);
 
 	style.css();
 }
--- js/html.js
+++ js/html.js
@@ -2,6 +2,112 @@
 
 export class MyHTML {
 
+	button(element, css, text, click=null) {
+
+		let item = document.createElement("button");
+		if (css !== null) { item.classList.add(...css); }
+
+		item.innerHTML = text;
+		item.addEventListener("click", click);
+
+		element.appendChild(item);
+
+		return item;
+	}
+
+	div(element, css=null, text="") {
+
+		let item = document.createElement("div");
+		if (css !== null) { item.classList.add(...css); }
+
+		item.innerHTML = text;
+
+		element.appendChild(item);
+
+		return item;
+	}
+
+	input(element, css, size, readonly=false) {
+
+		let item = document.createElement("input");
+		if (css !== null) { item.classList.add(...css); }
+
+		item.type = "text";
+		item.autocomplete = "off";
+		item.autocorrect = "off";
+		item.autocapitalize = "off";
+		item.size = size;
+		if (readonly === true) { item.readOnly = "readonly"; }
+
+		element.appendChild(item);
+
+		return item;
+	}
+
+	number(element, css, text) {
+
+		let item = document.createElement("span");
+		if (css !== null) { item.classList.add(...css); }
+
+		item.innerHTML = text;
+
+		element.appendChild(item);
+
+		return item;
+	}
+
+	options(element, choose, keys) {
+
+		let len = element.children.length;
+		for (let i=0; i<len; i++) {
+			element.remove(0);
+		}
+
+		for (let i=0; i<choose.length; i++) {
+	    	const option = document.createElement("option");
+	    	if (keys == true) {
+		    	option.textContent = choose[i].name;
+				option.value = choose[i].key;
+			}
+	    	else {
+		    	option.textContent = choose[i].group;
+				option.value = choose[i].group;
+	    	}
+	    	element.appendChild(option);
+	    }
+
+	    return element;
+	}
+
+	select(element, css, change=null) {
+
+		let item = document.createElement("select");
+		if (css !== null) { item.classList.add(...css); }
+
+		item.innerHTML = "";
+		item.addEventListener("change", change);
+
+		element.appendChild(item);
+
+		return item;
+	}
+
+	span(element, css=null, text="", style=null) {
+
+		let item = document.createElement("span");
+		if (css !== null) { item.classList.add(...css); }
+
+		item.innerHTML = text;
+		if (style !== null) {
+			for (let i=0; i<style.length; i++) {
+				item.style.setProperty(style[i][0], style[i][1]);
+			}
+		}
+
+		element.appendChild(item);
+
+		return item;
+	}
 }
 
 export class MyEntry {
